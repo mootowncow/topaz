7ad5bb7a9391c8d058048c35594db0cf40efae0b
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-05-08 14:26:17 -0400

    (TP moves) Winds of promy dispellable check

--- scripts/globals/mobskills/winds_of_promyvion.lua
+++ scripts/globals/mobskills/winds_of_promyvion.lua
@@ -9,10 +9,14 @@ require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/msg")
+require("scripts/globals/utils")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-    return 0
+    if utils.hasDispellableEffect(target) then
+        return 0
+    end
+    return 1
 end
 
 function onMobWeaponSkill(target, mob, skill)
