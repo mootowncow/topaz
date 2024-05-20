f628b36a0e22461e9ae46df07b0f479af70377d1
Shiyo <strikinginsanity@hotmail.com>
2024-05-19 23:53:01 -0400

    (WS) Leaden MACC bonus
    +100 MACC bonus added

--- scripts/globals/weaponskills/leaden_salute.lua
+++ scripts/globals/weaponskills/leaden_salute.lua
@@ -27,6 +27,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.skill = tpz.skill.MARKSMANSHIP
     params.includemab = true
 	params.enmityMult = 0.5
+    params.bonusmacc = 100
 
     if USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.ftp200 = 6.7 params.ftp300 = 10.0
