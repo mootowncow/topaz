c5ae1b7985d1089b35da37a2d0067864f29666f6
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-05-24 14:01:01 -0400

    (Pathing) Shikigami weapon pathing fix

--- scripts/zones/RoMaeve/mobs/Shikigami_Weapon.lua
+++ scripts/zones/RoMaeve/mobs/Shikigami_Weapon.lua
@@ -7,40 +7,40 @@ require("scripts/globals/regimes")
 require("scripts/globals/status")
 -----------------------------------
 
-local path =
-{
-    -47, -4, -37,
-    -49, -4, -37,
-    -54, -4, -37,
-    -59, -4, -43,
-    -67, -3.7, -50.6,
-    -76, -1.4, -60,
-    -87, -1, -69,
-    -104, -3, -58,
-    -118, -3, -46,
-    -112, -3.5, -28,
-    -98, -6, -16,
-    -84, -6, -9,
-    -64, -6, 1.1,
-    -40, -6, 9.6,
-    -20, -6, 12,
-    -10, -6.2, 11,
-    31, -6, 11,
-    52, -6, 5,
-    75, -6, -4,
-    94, -6, -14,
-    110, -4.2, -25,
-    118, -3, -34,
-    109, -3.25, -55,
-    87, -1, -70,
-    68, -3.3, -53,
-    57, -4, -41,
-    28, -4, -37,
-    6, -4, -35,
-    -15, -4, -36,
-    -23, -4, -36,
-    -35, -4, -36,
+local pathingTable = {
+    { x = -47, y = -4, z = -37 },
+    { x = -49, y = -4, z = -37 },
+    { x = -54, y = -4, z = -37 },
+    { x = -59, y = -4, z = -43 },
+    { x = -67, y = -3.7, z = -50.6 },
+    { x = -76, y = -1.4, z = -60 },
+    { x = -87, y = -1, z = -69 },
+    { x = -104, y = -3, z = -58 },
+    { x = -118, y = -3, z = -46 },
+    { x = -112, y = -3.5, z = -28 },
+    { x = -98, y = -6, z = -16 },
+    { x = -84, y = -6, z = -9 },
+    { x = -64, y = -6, z = 1.1 },
+    { x = -40, y = -6, z = 9.6 },
+    { x = -20, y = -6, z = 12 },
+    { x = -10, y = -6.2, z = 11 },
+    { x = 31, y = -6, z = 11 },
+    { x = 52, y = -6, z = 5 },
+    { x = 75, y = -6, z = -4 },
+    { x = 94, y = -6, z = -14 },
+    { x = 110, y = -4.2, z = -25 },
+    { x = 118, y = -3, z = -34 },
+    { x = 109, y = -3.25, z = -55 },
+    { x = 87, y = -1, z = -70 },
+    { x = 68, y = -3.3, z = -53 },
+    { x = 57, y = -4, z = -41 },
+    { x = 28, y = -4, z = -37 },
+    { x = 6, y = -4, z = -35 },
+    { x = -15, y = -4, z = -36 },
+    { x = -23, y = -4, z = -36 },
+    { x = -35, y = -4, z = -36 },
 }
+
 
 function onMobInitialize(mob)
 end
@@ -58,18 +58,12 @@ function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
     mob:addImmunity(tpz.immunity.SILENCE)
     mob:setStatus(tpz.status.INVISIBLE)
-    onMobRoam(mob)
-end
-
-function onPath(mob)
-    tpz.path.patrol(mob, path, tpz.path.flag.RUN)
+    mob:setLocalVar("path", 0)
+    mob:setLocalVar("pathstep", 0)
 end
 
 function onMobRoam(mob)
-    -- move to start position if not moving
-    if not mob:isFollowingPath() then
-        mob:pathThrough(tpz.path.first(path), tpz.path.flag.RUN)
-    end
+    tpz.path.loop(mob, pathingTable, tpz.path.flag.RUN)
 end
 
 function onMobEngaged(mob, target)
