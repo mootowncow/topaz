30b5963ad8775bfecf0a3c7c498f4b483fb632b9
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-05-27 11:38:56 -0400

    (WHM) Sacrifice + Solace logic

--- scripts/globals/spells/sacrifice.lua
+++ scripts/globals/spells/sacrifice.lua
@@ -13,31 +13,40 @@ function onMagicCastingCheck(caster, target, spell)
 end
 
 function onSpellCast(caster, target, spell)
-    local count = 1
     local removables = utils.GetRemovableEffects()
+    local hasSolace = caster:hasStatusEffect(tpz.effect.AFFLATUS_SOLACE)
+    local maxRemovals = hasSolace and 7 or 1  -- Remove up to 7 effects if hasSolace is true, otherwise just 1
+    local removals = 0
 
     -- remove one effect and add it to me
     for i, effect in ipairs(removables) do
+        if removals >= maxRemovals then
+            break
+        end
 
-        if (target:hasStatusEffect(effect)) then
+        if target:hasStatusEffect(effect) then
             local currentEffect = target:getStatusEffect(effect)
             local effectFlags = currentEffect:getFlag()
-            if (bit.band(effectFlags, tpz.effectFlag.WALTZABLE) ~= 0) or (currentEffect == tpz.effect.PETRIFICATION) then
+            if bit.band(effectFlags, tpz.effectFlag.WALTZABLE) ~= 0 or currentEffect:getType() == tpz.effect.PETRIFICATION then
                 spell:setMsg(tpz.msg.basic.MAGIC_ABSORB_AILMENT)
-
+                
                 local statusEffect = target:getStatusEffect(effect)
 
                 -- only add it to me if I don't have it
-                if (caster:hasStatusEffect(effect) == false) then
+                if not caster:hasStatusEffect(effect) then
                     caster:addStatusEffect(effect, statusEffect:getPower(), statusEffect:getTickCount(), statusEffect:getDuration())
                 end
 
                 target:delStatusEffectSilent(effect)
-                return 1
+                removals = removals + 1
             end
         end
     end
 
-    spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT) -- no effect
-    return 0
+    if removals == 0 then
+        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT) -- no effect
+    end
+
+    return removals
 end
+
