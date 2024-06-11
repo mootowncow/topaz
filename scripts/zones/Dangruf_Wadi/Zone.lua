2d828b4f74bfeff99ba98c6ab497d443798fec50
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-06-11 12:58:54 -0400

    (Doors / Triggers)Dangruf Wadi Geyser fixes

--- scripts/zones/Dangruf_Wadi/Zone.lua
+++ scripts/zones/Dangruf_Wadi/Zone.lua
@@ -11,9 +11,9 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onInitialize(zone)
-    zone:registerRegion(1, -133.5, 2, 132.6, -132.7, 4,  133.8)  -- I-8 Geyser
-    zone:registerRegion(2, -213.5, 2,  92.6, -212.7, 4,   94.0)  -- H-8 Geyser
-    zone:registerRegion(3,  -67.3, 2, 532.8,  -66.3, 4,  534.0)  -- J-3 Geyser
+    zone:registerRegion(1, -133.1, 2.6, 133.2, 0, 0, 0)  -- I-8 Geyser
+    zone:registerRegion(2, -213.3, 2.9, 93.4, 0, 0, 0)  -- H-8 Geyser
+    zone:registerRegion(3, -66.7, 2.9, 533.4,  0, 0, 0)  -- J-3 Geyser
 
     tpz.treasure.initZone(zone)
 end
@@ -34,14 +34,17 @@ function onRegionEnter(player, region)
     switch (region:GetRegionID()): caseof
     {
         [1] = function (x)
+            player:setPos(-133.19, 2.66, 133.20)
             player:startEvent(10)
             SendEntityVisualPacket(ID.npc.GEYSER_OFFSET, "kkj2")
         end,
         [2] = function (x)
+            player:setPos(-213.3, 2.9, 93.4)
             player:startEvent(11)
             SendEntityVisualPacket(ID.npc.GEYSER_OFFSET + 1, "kkj1")
         end,
         [3] = function (x)
+        player:setPos(-66.7, 2.9, 533)
             player:startEvent(12)
             SendEntityVisualPacket(ID.npc.GEYSER_OFFSET + 2, "kkj3")
         end,
