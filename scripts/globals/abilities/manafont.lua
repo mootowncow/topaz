2b03fec8ed9d0de1b4f2aa796dc8b8414f0276a0
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-06-11 13:23:44 -0400

    (BLM) Manafont changed to 3m duration

--- scripts/globals/abilities/manafont.lua
+++ scripts/globals/abilities/manafont.lua
@@ -3,7 +3,7 @@
 -- Eliminates the cost of magic spells.
 -- Obtained: Black Mage Level 1
 -- Recast Time: 1:00:00
--- Duration: 0:01:00
+-- Duration: 0:03:00
 -----------------------------------
 require("scripts/globals/settings")
 require("scripts/globals/status")
@@ -14,5 +14,5 @@ function onAbilityCheck(player, target, ability)
 end
 
 function onUseAbility(player, target, ability)
-    player:addStatusEffect(tpz.effect.MANAFONT, 1, 0, 60)
+    player:addStatusEffect(tpz.effect.MANAFONT, 1, 0, 180)
 end
