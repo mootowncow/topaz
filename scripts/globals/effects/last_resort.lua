569abb90d03468ccae7280231570bfec79dc7261
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-06-28 12:03:24 -0400

    (DRK) Last Resort JA haste 2h only
    Now requires a 2h weapon to get the JA haste

--- scripts/globals/effects/last_resort.lua
+++ scripts/globals/effects/last_resort.lua
@@ -11,7 +11,7 @@ function onEffectGain(target, effect)
 
     target:addMod(tpz.mod.ATT, jpValue)
     target:addMod(tpz.mod.ATTP, 25 + target:getMerit(tpz.merit.LAST_RESORT_EFFECT))
-    target:addMod(tpz.mod.HASTE_ABILITY, target:getMod(tpz.mod.DESPERATE_BLOWS) + target:getMerit(tpz.merit.DESPERATE_BLOWS))
+    target:addMod(tpz.mod.TWOHAND_HASTE_ABILITY, target:getMod(tpz.mod.DESPERATE_BLOWS) + target:getMerit(tpz.merit.DESPERATE_BLOWS))
 
     -- Gear that affects this mod is handled by a Latent Effect because the gear must remain equipped
     target:addMod(tpz.mod.DEFP, -25 - target:getMerit(tpz.merit.LAST_RESORT_EFFECT))
@@ -25,7 +25,7 @@ function onEffectLose(target, effect)
 
     target:delMod(tpz.mod.ATT, jpValue)
     target:delMod(tpz.mod.ATTP, 25 + target:getMerit(tpz.merit.LAST_RESORT_EFFECT))
-    target:delMod(tpz.mod.HASTE_ABILITY, target:getMod(tpz.mod.DESPERATE_BLOWS) + target:getMerit(tpz.merit.DESPERATE_BLOWS))
+    target:delMod(tpz.mod.TWOHAND_HASTE_ABILITY, target:getMod(tpz.mod.DESPERATE_BLOWS) + target:getMerit(tpz.merit.DESPERATE_BLOWS))
      -- Gear that affects this mod is handled by a Latent Effect because the gear must remain equipped
     target:delMod(tpz.mod.DEFP, -25 - target:getMerit(tpz.merit.LAST_RESORT_EFFECT))
 end
