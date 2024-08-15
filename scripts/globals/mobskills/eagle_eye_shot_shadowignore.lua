f642698ac149ba2996c1db2352d8e86d59637193
Shiyo <strikinginsanity@hotmail.com>
2024-08-14 23:23:41 -0400

    (TP Moves) Misc fix

--- scripts/globals/mobskills/eagle_eye_shot_shadowignore.lua
+++ scripts/globals/mobskills/eagle_eye_shot_shadowignore.lua
@@ -27,7 +27,7 @@ function onMobWeaponSkill(target, mob, skill)
     params_phys.mnd_wsc = 0.0
     params_phys.chr_wsc = 0.0
     local info = MobRangedMove(mob, target, skill, numhits, accmod, dmgmod, TP_RANGED, params_phys, 2, 2)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.RANGED, tpz.damageType.NONE, MOBPARAM_IGNORE_SHADOWS)
-    target:takeDamage(dmg, mob, tpz.attackType.RANGED, tpz.damageType.NONE)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.RANGED, tpz.damageType.RANGED, MOBPARAM_IGNORE_SHADOWS)
+    target:takeDamage(dmg, mob, tpz.attackType.RANGED, tpz.damageType.RANGED)
     return dmg
 end
