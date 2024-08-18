15f6f3db4787bdd3cbcb8a7a0b8f140a09f4e73e
Shiyo <strikinginsanity@hotmail.com>
2024-08-18 16:45:49 -0400

    (SCNM) Fixes

--- scripts/globals/mobskills/sticky_grenade.lua
+++ scripts/globals/mobskills/sticky_grenade.lua
@@ -21,7 +21,6 @@ function onMobWeaponSkill(target, mob, skill)
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.FIRE, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
-    if dmg > 0 then target:setTP(0) end
     MobStatusEffectMove(mob, target, tpz.effect.SLOW, 5000, 0, 300)
     MobStatusEffectMove(mob, target, tpz.effect.WEIGHT, 25, 0, 300)
     return dmg
