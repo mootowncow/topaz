9247c07b2f3f2abb117a1a029f4445a34aadb6f7
Shiyo <strikinginsanity@hotmail.com>
2024-08-18 16:58:37 -0400

    (TP Moves) Fixes

--- scripts/globals/mobskills/flame_arrow.lua
+++ scripts/globals/mobskills/flame_arrow.lua
@@ -14,25 +14,10 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local numhits = 1
-    local accmod = 1
-    local dmgmod = 2
-    local params_phys = {}
-    params_phys.multiplier = dmgmod
-    params_phys.tp150 = 1
-    params_phys.tp300 = 1
-    params_phys.str_wsc = 0.16
-    params_phys.dex_wsc = 0.0
-    params_phys.vit_wsc = 0.0
-    params_phys.agi_wsc = 0.15
-    params_phys.int_wsc = 0.0
-    params_phys.mnd_wsc = 0.0
-    params_phys.chr_wsc = 0.0
+    local dmgmod = 3
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.FIRE, dmgmod, TP_NO_EFFECT)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
 
-    local info = MobRangedMove(mob, target, skill, numhits, accmod, dmgmod, TP_RANGED, params_phys)
-
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.RANGED, tpz.damageType.FIRE, info.hitslanded)
-
-    target:takeDamage(dmg, mob, tpz.attackType.RANGED, tpz.damageType.FIRE)
+    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
     return dmg
 end
