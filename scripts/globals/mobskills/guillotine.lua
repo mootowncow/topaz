402642dc41783767935d595f907bbe3a551c568e
Shiyo <strikinginsanity@hotmail.com>
2024-08-20 00:14:05 -0400

    (TP Moves) Guillotine silence fix

--- scripts/globals/mobskills/guillotine.lua
+++ scripts/globals/mobskills/guillotine.lua
@@ -32,10 +32,8 @@ function onMobWeaponSkill(target, mob, skill)
     params_phys.chr_wsc = 0.0
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT, params_phys)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
-
-    -- 242 to a NIN, but shadows ate some hits...
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
-    MobStatusEffectMove(mob, target, tpz.effect.SILENCE, 1, 0, 300)
+    MobPhysicalStatusEffectMove(mob, target, skill, tpz.effect.SILENCE, 1, 0, 300)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
 end
