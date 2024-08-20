925aeaf8b776dd8cf4714346cc81c5c1a34af234
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-08-20 15:51:39 -0400

    (GM Commands) Add speed to target

--- scripts/commands/speed.lua
+++ scripts/commands/speed.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------------------------------
 -- func: speed
--- desc: Sets the players movement speed.
+-- desc: Sets the targets movement speed.
 ---------------------------------------------------------------------------------------------------
 require("scripts/globals/status")
 require("scripts/globals/settings")
@@ -17,36 +17,43 @@ function error(player, msg)
 end
 
 function onTrigger(player, speed)
+    local targ = player:getCursorTarget()
+
     if not speed then
         player:PrintToPlayer(string.format('Current Speed: %u', player:getSpeed()))
-
         return
     end
 
-    -- Validate speed amount
-    if speed < 0 or speed > 255 then
-        error(player, 'Invalid speed amount.')
+    if (targ:isMob() or targ:isPet() or targ:isPC()) then
 
-        return
-    end
+        -- Validate speed amount
+        if speed < 0 or speed > 255 then
+            error(player, 'Invalid speed amount.')
 
-    -- Bypass speed and inform player.
-    local baseSpeed = speed
-
-    if speed == 0 then
-        if player:hasStatusEffect(tpz.effect.MOUNTED) then
-            baseSpeed = -20
-        else
-            baseSpeed = 40
+            return
         end
 
-        player:PrintToPlayer('Returning to your regular speed.')
-    else
-        player:PrintToPlayer('Bypassing regular speed calculations and limits.')
-        player:PrintToPlayer('Set speed value to "0" to return to your regular speed.')
-        player:PrintToPlayer(string.format('New speed: %u', speed))
-    end
+        -- Bypass speed and inform player.
+        local baseSpeed = speed
 
-    player:setMod(tpz.mod.MOVE_SPEED_OVERIDE, speed)
-    player:speed(baseSpeed)
+        if speed == 0 then
+            if player:hasStatusEffect(tpz.effect.MOUNTED) then
+                baseSpeed = -20
+            else
+                baseSpeed = 40
+            end
+
+            player:PrintToPlayer('Returning to your regular speed.')
+        else
+            player:PrintToPlayer('Bypassing regular speed calculations and limits.')
+            player:PrintToPlayer('Set speed value to "0" to return to your regular speed.')
+            player:PrintToPlayer(string.format("%s's new speed: %u", targ:getName(), speed))
+        end
+
+        targ:setMod(tpz.mod.MOVE_SPEED_OVERIDE, speed)
+        targ:speed(baseSpeed)
+    else
+        error(player, "Invalid target.")
+        return
+    end
 end
