43081a4b51c33e613a3818087a0c06faf552f805
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-08-23 12:46:50 -0400

    (WHM) Asylum fixes
    No longer applies per target hit, but buffed it's amount recovered to 20%(It was already recovering 20% duo anyways..)

--- scripts/globals/abilities/asylum.lua
+++ scripts/globals/abilities/asylum.lua
@@ -1,7 +1,7 @@
 -----------------------------------
 -- Ability: Asylum
 -- Description: Grants party members a powerful resistance to enfeebling magic and Dispel effects.
--- Also restores 10% of the WHM's MP if a party member is in range and afflatus Misery is active
+-- Also restores 20% of the WHM's MP if a party member is in range and afflatus Misery is active
 -- Obtained: WHM Level 75
 -- Recast Time: 00:01:00
 -- Duration: 0:01:00
@@ -16,24 +16,26 @@ end
 
 function onUseAbility(player, target, ability)
     local party = player:getParty()
-    local mpAdded = false
+    local asylumMPRecovery = player:getLocalVar("asylumMPRecovery")
 
-    if player:hasStatusEffect(tpz.effect.AFFLATUS_MISERY) then
-        local NearbyEntities = player:getNearbyEntities(10)
-        if NearbyEntities == nil then return end
-        if NearbyEntities then
-            for _,entity in pairs(NearbyEntities) do
-                if entity:isAlive() and
-                (entity:getAllegiance() == player:getAllegiance())
-                and (player:getID() ~= entity:getID()) then
-                    if not mpAdded then
-                        player:addMP((player:getMaxMP()/100)*10)
-                        mpAdded = true
+    if (os.time() >= asylumMPRecovery) then -- So that it never recovers MP more than once per use
+        if player:hasStatusEffect(tpz.effect.AFFLATUS_MISERY) then
+            local NearbyEntities = player:getNearbyEntities(10)
+            if NearbyEntities == nil then return end
+            if NearbyEntities then
+                for _,entity in pairs(NearbyEntities) do
+                    if entity:isAlive() and
+                    (entity:getAllegiance() == player:getAllegiance())
+                    and (player:getID() ~= entity:getID()) then
+                    local mprecovered = (player:getMaxMP()/100)*20
+                        player:addMP(mprecovered)
+                        player:setLocalVar("asylumMPRecovery", os.time() + 15)
+                        break
                     end
-                    break
                 end
             end
         end
     end
+
     target:addStatusEffect(tpz.effect.ASYLUM, 0, 0, 60)
 end
