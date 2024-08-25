91696f07705127dbc0f918b8122bbe32bebcfd11
Shiyo <strikinginsanity@hotmail.com>
2024-08-24 20:26:11 -0400

    (Mixins) Missing worm Mixin

---
+++ scripts/zones/Western_Altepa_Desert/mobs/Desert_Worm.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: Western Altepa Desert
+--  Mob: Desert Worm
+-----------------------------------
+mixins = {require("scripts/mixins/families/worm")}
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller, noKiller)
+end
