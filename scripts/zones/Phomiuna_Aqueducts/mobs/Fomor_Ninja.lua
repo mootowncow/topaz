fbd7dfc465078b888d20a1643aeb07f5da9674d9
Shiyo <strikinginsanity@hotmail.com>
2024-08-31 12:59:10 -0400

    (Mobs)Fomor Ninja stats

--- scripts/zones/Phomiuna_Aqueducts/mobs/Fomor_Ninja.lua
+++ scripts/zones/Phomiuna_Aqueducts/mobs/Fomor_Ninja.lua
@@ -6,8 +6,6 @@ mixins = {require("scripts/mixins/fomor_hate")}
 require("scripts/globals/mobs")
 
 function onMobSpawn(mob)
-    mob:setDelay(3600)
-    mob:setMod(tpz.mod.DOUBLE_ATTACK, 100)
 end
 
 function onMobDeath(mob, player, isKiller, noKiller)
