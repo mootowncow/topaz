094169848c4eb54e15b48ecd1d84889ba6905baa
Shiyo <strikinginsanity@hotmail.com>
2024-09-01 15:37:40 -0400

    (Trusts) Tuning

--- scripts/globals/mobskills/binding_microtube.lua
+++ scripts/globals/mobskills/binding_microtube.lua
@@ -20,7 +20,7 @@ function onMobWeaponSkill(target, mob, skill)
     local tick = 0
     local duration = 60
 
-    local dmgmod = 4
+    local dmgmod = 2
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*1, tpz.magic.ele.NONE, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.ELEMENTAL, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.ELEMENTAL)
