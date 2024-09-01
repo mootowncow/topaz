e24e1186d5c284adb81dc0854449e1f2790e14a2
Shiyo <strikinginsanity@hotmail.com>
2024-09-01 16:20:14 -0400

    (Mixins) Worm Mixin

---
+++ scripts/zones/Eastern_Altepa_Desert/mobs/Flesh_Eater.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: Eastern Altepa Desert
+--  Mob: Flesh Eater
+-----------------------------------
+mixins = {require("scripts/mixins/families/worm")}
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller, noKiller)
+end
