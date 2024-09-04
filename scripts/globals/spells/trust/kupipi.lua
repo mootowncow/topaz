c9973995bf9af1e9da4cd1431513761d233d8b2f
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-09-04 14:49:58 -0400

    (Trusts) More adjustments
    Koru-moru and Kupipi will no longer stay in melee range
    
    Elivira will now stay 16 yalms away

--- scripts/globals/spells/trust/kupipi.lua
+++ scripts/globals/spells/trust/kupipi.lua
@@ -56,6 +56,7 @@ function onMobSpawn(mob)
     mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, tpz.effect.PARALYSIS, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.PARALYZE, 60)
     mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, tpz.effect.SLOW, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.SLOW, 60)
 
+    mob:setMobMod(tpz.mobMod.TRUST_DISTANCE, tpz.trust.movementType.NO_MOVE)
     tpz.trust.onMobSpawn(mob)
 end
 
