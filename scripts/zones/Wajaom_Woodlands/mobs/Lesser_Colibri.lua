72f1e6d078ecf95810f4d03b0bee2075ce5867d0
Shiyo <strikinginsanity@hotmail.com>
2024-09-04 17:05:07 -0400

    (Mixins) Remove Colibri mixin off Lessers
    https://ffxiclopedia.fandom.com/wiki/Lesser_Colibri

--- scripts/zones/Wajaom_Woodlands/mobs/Lesser_Colibri.lua
+++ scripts/zones/Wajaom_Woodlands/mobs/Lesser_Colibri.lua
@@ -3,9 +3,9 @@
 --  Mob: Lesser Colibri
 -- Note: Place holder Zoraal Ja's Pkuucha
 -----------------------------------
-mixins = {require("scripts/mixins/families/colibri_mimic")}
 local ID = require("scripts/zones/Wajaom_Woodlands/IDs")
 require("scripts/globals/mobs")
+-----------------------------------
 
 function onMobDeath(mob, player, isKiller, noKiller)
 	if isKiller and math.random(1,100) <= 24 then 
