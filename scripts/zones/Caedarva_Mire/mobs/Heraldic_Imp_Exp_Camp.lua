aac0a3dcc9449b737f3330a025f84d28d4530d27
Shiyo <strikinginsanity@hotmail.com>
2024-09-06 22:13:01 -0400

    (Mixins) Missing imp mixin

---
+++ scripts/zones/Caedarva_Mire/mobs/Heraldic_Imp_Exp_Camp.lua
@@ -0,0 +1,8 @@
+-----------------------------------
+-- Area: Caedarva Mire
+--  Mob: Heraldic Imp (EXP camp by Azouph Staging Point)
+-----------------------------------
+mixins = {require("scripts/mixins/families/imp")}
+-----------------------------------
+function onMobDeath(mob, player, isKiller, noKiller)
+end
