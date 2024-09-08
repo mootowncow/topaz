c8b687dd8c6646f173a10874950cfa6b306366b2
Shiyo <strikinginsanity@hotmail.com>
2024-09-08 17:01:57 -0400

    (Trusts) Tuning

--- scripts/globals/mobskills/mandalic_stab.lua
+++ scripts/globals/mobskills/mandalic_stab.lua
@@ -27,9 +27,9 @@ function onMobWeaponSkill(target, mob, skill)
     params_phys.int_wsc = 0.0
     params_phys.mnd_wsc = 0.0
     params_phys.chr_wsc = 0.0
+    params_phys.attack_boost = 175
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT, params_phys)
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,tpz.attackType.PHYSICAL,tpz.damageType.SLASHING, MOBPARAM_IGNORE_SHADOWS)
-
    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
    if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then target:tryInterruptSpell(mob, info.hitslanded) end
    return dmg
