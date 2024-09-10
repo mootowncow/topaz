79cfef83529b243299bdaabd11ecfd4c8a94e317
Shiyo <strikinginsanity@hotmail.com>
2024-09-09 23:18:26 -0400

    TODO

---
+++ scripts/zones/Mount_Zhayolm/mobs/Assassin_Fly.lua
@@ -0,0 +1,11 @@
+-----------------------------------
+-- Area: Mount Zhayolm
+--   NM: Assassin Fly
+-- Two babies(Tinier)
+-- All link if one is pulled, give same experience, same HP
+-----------------------------------
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+-----------------------------------
+function onMobDeath(mob)
+end
