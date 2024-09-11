a9f27a2f89018ca081f851fad2af0f4a3960a97b
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-09-11 15:45:49 -0400

    (SMN) Fix targeting of Healing Ruby

--- scripts/globals/abilities/pets/healing_ruby.lua
+++ scripts/globals/abilities/pets/healing_ruby.lua
@@ -12,7 +12,5 @@ function onAbilityCheck(player, target, ability)
     return 0, 0
 end
 function onPetAbility(target, pet, skill)
-    local master = pet:getMaster()
-    local target = master:getCursorTarget()
     return AvatarHealBP(pet, target, skill, 0.10, false)
 end
