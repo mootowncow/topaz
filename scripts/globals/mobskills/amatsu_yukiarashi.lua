36f536e32ce155b6b4099d527a97205e0c5e2e25
Shiyo <strikinginsanity@hotmail.com>
2024-09-12 17:08:02 -0400

    (Trusts) Tenzen WS corrections

--- scripts/globals/mobskills/amatsu_yukiarashi.lua
+++ scripts/globals/mobskills/amatsu_yukiarashi.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------
---  Amatsu: Yukiarashi
+--  Amatsu: Kazakiri 
 --
---  Description:  Blinds target. Damage varies with TP.
+--  Description:  Tachi: Jinpu variant, ~200
 --  Type: Physical
 --  Shadow per hit
 --  Range: Melee
@@ -16,25 +16,23 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local numhits = 1
+    local numhits = 2
     local accmod = 1
-    local dmgmod = 2
+    local dmgmod = 1
     local params_phys = {}
     params_phys.multiplier = dmgmod
     params_phys.tp150 = 1
     params_phys.tp300 = 1
-    params_phys.str_wsc = 0.75
+    params_phys.str_wsc = 0.3
     params_phys.dex_wsc = 0.0
     params_phys.vit_wsc = 0.0
     params_phys.agi_wsc = 0.0
     params_phys.int_wsc = 0.0
     params_phys.mnd_wsc = 0.0
     params_phys.chr_wsc = 0.0
-    params_phys.attack_boost = 150
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_ATK_VARIES, params_phys, 1.5, 1.5)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT, params_phys)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WIND, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
-	MobPhysicalStatusEffectMove(mob, target, skill, tpz.effect.BLINDNESS, 25, 0, 300)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
 end
