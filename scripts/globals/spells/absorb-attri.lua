8f77a37d0d14c2b434c5ada5b7473e0a2a9d408c
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-09-16 11:44:55 -0400

    (DRK) Absorb Attribute

--- scripts/globals/spells/absorb-attri.lua
+++ scripts/globals/spells/absorb-attri.lua
@@ -19,18 +19,43 @@ function onSpellCast(caster, target, spell)
     params.skillType = tpz.skill.DARK_MAGIC
     params.bonus = 175
     local resist = applyResistanceEffect(caster, target, spell, params)
-    local stolen = 0
+    local effectsToSteal = 0
+    local stolenEffects = 0
+    local effect = nil
 
+    -- Check if the spell landed..
     if resist >= 0.5 then
-        stolen = caster:stealStatusEffect(target)
-        if stolen ~= 0 then
-            spell:setMsg(tpz.msg.basic.MAGIC_STEAL)
-        else
-            spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
-        end
-    else
-        spell:setMsg(tpz.msg.basic.MAGIC_RESIST)
+        effectsToSteal = effectsToSteal +1
     end
 
-    return stolen
+    -- Nether void adds additional dispels
+    -- TODO: JP Adds even more dispels to Nether Void!
+    if caster:hasStatusEffect(tpz.effect.NETHER_VOID) then
+        resist = applyResistanceEffect(caster, target, spell, params)
+        if resist >= 0.5 then
+            effectsToSteal = effectsToSteal +1
+        end
+    end
+
+    -- Attempt to steal status effects based on how many are to be stolen
+    while stolenEffects < effectsToSteal do
+        effect = caster:stealStatusEffect(target)
+        if effect ~= 0 then
+            stolenEffects = stolenEffects + 1
+        else
+            break -- No more dispellable effects to steal
+        end
+    end
+
+    if stolenEffects > 0 then
+        spell:setMsg(tpz.msg.basic.MAGIC_STEAL)
+    elseif effectsToSteal > 0 then
+        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT) -- Resisted or no effects to steal
+    else
+        spell:setMsg(tpz.msg.basic.MAGIC_RESIST) -- All dispel resist checks failed
+    end
+
+    caster:delStatusEffectSilent(tpz.effect.NETHER_VOID)
+
+    return stolenEffects
 end
