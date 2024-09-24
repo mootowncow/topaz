07fe892e65619cc591751dfd9f02ae4bca18033e
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-09-24 12:06:02 -0400

    (SMN) Nether Blast MP cost

--- scripts/globals/abilities/pets/nether_blast.lua
+++ scripts/globals/abilities/pets/nether_blast.lua
@@ -30,8 +30,6 @@ function onPetAbility(target, pet, skill)
 
     local damage = AvatarMagicalBP(pet, target, skill, tpz.magic.ele.DARK, params, NONE, 0)
     dmg = AvatarMagicalFinalAdjustments(damage, pet, skill, target, tpz.attackType.BREATH, tpz.magic.ele.DARK, params)
-    local master = pet:getMaster()
-    master:delMP(109)
 
     return dmg
 end
