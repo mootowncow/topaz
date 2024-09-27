514b9558fa6956a842f4478fcf8b7ca4592abf3e
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-09-27 12:45:57 -0400

    (RNG) Velocity shot gives base 15% ranged delay reduction

--- scripts/globals/effects/velocity_shot.lua
+++ scripts/globals/effects/velocity_shot.lua
@@ -11,7 +11,7 @@ function onEffectGain(target, effect)
     target:addMod(tpz.mod.ATTP, -15)
     target:addMod(tpz.mod.HASTE_ABILITY, -1500)
     target:addMod(tpz.mod.RATTP, 15)
-    target:addMod(tpz.mod.RANGED_DELAYP, -10)
+    target:addMod(tpz.mod.RANGED_DELAYP, -15)
 end
 
 function onEffectTick(target, effect)
@@ -24,5 +24,5 @@ function onEffectLose(target, effect)
     target:delMod(tpz.mod.ATTP, -15)
     target:delMod(tpz.mod.HASTE_ABILITY, -1500)
     target:delMod(tpz.mod.RATTP, 15)
-    target:delMod(tpz.mod.RANGED_DELAYP, -10)
+    target:delMod(tpz.mod.RANGED_DELAYP, -15)
 end
