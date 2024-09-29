02cd484f3d6063fa1734bed110a14a5aa4ec3ffe
Shiyo <strikinginsanity@hotmail.com>
2024-09-29 17:27:35 -0400

    (Trusts) Elivira adjustments

--- scripts/globals/spells/trust/elivira.lua
+++ scripts/globals/spells/trust/elivira.lua
@@ -25,7 +25,7 @@ function onMobSpawn(mob)
 
     mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, tpz.effect.BARRAGE, ai.r.JA, ai.s.SPECIFIC, tpz.ja.BARRAGE)
 
-    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, tpz.effect.DOUBLE_SHOT, ai.r.JA, ai.s.SPECIFIC, tpz.ja.DOUBLE_SHOT)
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, tpz.effect.BARRAGE, ai.r.JA, ai.s.SPECIFIC, tpz.ja.DOUBLE_SHOT)
 
     if mob:getMainLvl() >= 45 then
         mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, tpz.effect.VELOCITY_SHOT, ai.r.JA, ai.s.SPECIFIC, tpz.ja.VELOCITY_SHOT)
