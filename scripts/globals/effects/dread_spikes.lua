28a632f0b57c86289f68d275aa0404b3249081b7
Shiyo <strikinginsanity@hotmail.com>
2024-09-28 20:16:35 -0400

    (DRK) Temp bandaid Dread Spikes MACC

--- scripts/globals/effects/dread_spikes.lua
+++ scripts/globals/effects/dread_spikes.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.SPIKES, 3)
+    target:addMod(tpz.mod.SPIKES_MACC, 175)
 end
 
 function onEffectTick(target, effect)
@@ -15,5 +16,6 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.SPIKES, 3)
+    target:delMod(tpz.mod.SPIKES_MACC, 175)
     target:setCharVar("drkSpikes", 0)
 end
