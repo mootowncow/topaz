aa51f8dfab4800be6804ba70471801b06756a01f
Shiyo <strikinginsanity@hotmail.com>
2024-10-27 00:48:18 -0400

    Update Abatwa.lua

--- scripts/zones/Crawlers_Nest_[S]/mobs/Abatwa.lua
+++ scripts/zones/Crawlers_Nest_[S]/mobs/Abatwa.lua
@@ -3,7 +3,15 @@
 --   NM: Abatwa
 ------------------------------
 require("scripts/globals/hunts")
-------------------------------
+require("scripts/globals/status")
+-----------------------------------
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.TRIPLE_ATTACK, 35)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+end
+
+function onMobFight(mob, target)
+end
 
 function onMobDeath(mob, player, isKiller, noKiller)
     tpz.hunts.checkHunt(mob, player, 514)
