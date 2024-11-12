d3bb26c40a283be5358222bc30a2f6a54cd19070
ShiyoKozuki <strikinginsanity@hotmail.com>
2024-11-12 15:48:52 -0500

    JSEQuest fixes

--- scripts/globals/jsequests.lua
+++ scripts/globals/jsequests.lua
@@ -36,30 +36,27 @@ tpz.jsequest = tpz.jsequest or {}
 tpz.jsequest.onTrigger = function(player, npc, job)
     local stage = player:getCharVar("[JSEQUEST" .. job .. "]")
     local currentMerits = player:getMeritCount()
-    local totalMeritsGiven = player:getCharVar("[JSEQUEST" .. job .. "Merits]")
-    local meritsRemaining = 300 - player:getCharVar("[JSEQUEST" .. job .. "Merits]")
     local npcName = npc:getName()
     local isCorrectJob = player:getMainJob() == job
     local isLevelCapped = player:getMainLvl() >= 75
 
     -- If var is 1, then player has seen the intro quest text already, don't show again
     if isCorrectJob and isLevelCapped then
-        if (stage == 0) then
-            if questItems[job] and #questItems[job].items >= 2 then
-                local itemOne = GetItem(questItems[job].items[1])
-                local itemTwo = GetItem(questItems[job].items[2])
-                local itemOneName = string.gsub(itemOne:getName(), '_', ' ');
-                local itemTwoName = string.gsub(itemTwo:getName(), '_', ' ');
-
+        if questItems[job] and #questItems[job].items >= 2 then
+        local itemOne = GetItem(questItems[job].items[1])
+        local itemTwo = GetItem(questItems[job].items[2])
+        local itemOneName = string.gsub(itemOne:getName(), '_', ' ');
+        local itemTwoName = string.gsub(itemTwo:getName(), '_', ' ');
+            if (stage == 0) then
                 player:setCharVar("[JSEQUEST" .. job .. "]", 1)
                 player:PrintToPlayer("To show your mastery in your job, please bring me a " .. itemOneName .. " and a ".. itemTwoName .. ".", 0, npcName)
+            elseif (stage == 1) then
+                player:PrintToPlayer("You are on Stage 1. I require a " .. itemOneName .. " and a ".. itemTwoName .. ".", 0, npcName)
+            elseif (stage == 2) then
+                player:PrintToPlayer("You are on Stage 2. I still require " .. GetMeritsRemainingJSE(player) .. " merit points.", 0, npcName)
+            elseif (stage == 3) then
+                player:PrintToPlayer("You are on Stage 3. I require 5 Star Sapphires, 50 Scintillant Ingots, 50 Amph. Leather, 10 Divine Lumber", 0, npcName)
             end
-        elseif (stage == 1) then
-            player:PrintToPlayer("You are on stage 1. I require " .. itemOneName .. " and a ".. itemTwoName .. ".", 0, npcName)
-        elseif (stage == 2) then
-            player:PrintToPlayer("You are on stage 2. I still require " .. meritsRemaining .. " merit points.", 0, npcName)
-        elseif (stage == 3) then
-            player:PrintToPlayer("You are on stage 3. I require 5 Star Sapphires, 50 Scintillant Ingots, 50 Amph. Leather, 10 Divine Lumber", 0, npcName)
         end
     end
 end
@@ -67,7 +64,6 @@ end
 tpz.jsequest.onTrade = function(player, npc, trade, job)
     local stage = player:getCharVar("[JSEQUEST" .. job .. "]")
     local currentMerits = player:getMeritCount()
-    local totalMeritsGiven = player:getCharVar("[JSEQUEST" .. job .. "Merits]")
     local npcName = npc:getName()
     local isCorrectJob = player:getMainJob() == job
     local isLevelCapped = player:getMainLvl() >= 75
@@ -77,24 +73,19 @@ tpz.jsequest.onTrade = function(player, npc, trade, job)
             if npcUtil.tradeHasExactly(trade, { questItems[job].items[1], questItems[job].items[2] }) then
                 player:setCharVar("[JSEQUEST" .. job .. "]", 2)
                 player:PrintToPlayer("Fantastic job, now please bring me 300 merit points.", 0, npcName)
+                player:tradeComplete()
             end
         elseif (stage == 2) then
-            local meritsToAdd = currentMerits + totalMeritsGiven
+            local meritsToAdd = currentMerits + GetTotalMeritsGivenJSE(player)
             if meritsToAdd < 300 then
                 player:setMerits(0)
                 player:setCharVar("[JSEQUEST" .. job .. "Merits]", meritsToAdd)
-                player:setCharVar("[JSEQUEST" .. job .. "]", 3)
-                player:PrintToPlayer("You've gathered enough merit points!", 0, npcName)
-                player:PrintToPlayer("Now please bring me 25 imperial ingots", 0, npcName)
+                player:PrintToPlayer("You have turned in " .. GetTotalMeritsGivenJSE(player) .. " merits. I still require " .. GetMeritsRemainingJSE(player) .. " merits.", 0, npcName)
             else
-                local requiredMerits = 300 - totalMeritsGiven
-                    
-                if currentMerits >= requiredMerits then
-                    player:delMerits(requiredMerits)
-                else
-                    player:delMerits(currentMerits)
-                end
+                local requiredMerits = 300 - GetTotalMeritsGivenJSE(player)
+                local meritsToDeduct = math.min(currentMerits, requiredMerits)
 
+                player:setMerits(currentMerits - meritsToDeduct)
                 player:setCharVar("[JSEQUEST" .. job .. "Merits]", 300)
                 player:setCharVar("[JSEQUEST" .. job .. "]", 3)
                 player:PrintToPlayer("You've gathered enough merit points!", 0, npcName)
@@ -105,7 +96,18 @@ tpz.jsequest.onTrade = function(player, npc, trade, job)
                 player:PrintToPlayer("You have done well, here is your reward", 0, npcName)
                 player:setCharVar("[JSEQUEST" .. job .. "]", 4)
                 npcUtil.giveItem(player, questItems[job].reward[1])
+                player:tradeComplete()
             end
         end
     end
+end
+
+function GetTotalMeritsGivenJSE(player)
+    local totalMeritsGiven = player:getCharVar("[JSEQUEST" .. player:getMainJob() .. "Merits]")
+    return totalMeritsGiven
+end
+
+function GetMeritsRemainingJSE(player)
+    local meritsRemaining = 300 - player:getCharVar("[JSEQUEST" .. player:getMainJob() .. "Merits]")
+    return meritsRemaining
 end
