7eaff8311dace9d78432776449c96de2725b11b8
Shiyo <strikinginsanity@hotmail.com>
2024-12-01 13:49:45 -0500

    (SAM) Seigan counter rate

--- scripts/globals/effects/seigan.lua
+++ scripts/globals/effects/seigan.lua
@@ -8,8 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     local jpValue = target:getJobPointLevel(tpz.jp.SEIGAN_EFFECT) * 3
-    local counterRate = target:getMod(tpz.mod.ZANSHIN) / 4
-    counterRate = counterRate + target:getMod(tpz.mod.HASSO_SEIGAN_GIFT)
+    local counterRate = 10 + target:getMod(tpz.mod.HASSO_SEIGAN_GIFT)
 
     target:addMod(tpz.mod.DEF, jpValue)
 
@@ -23,8 +22,7 @@ end
 
 function onEffectLose(target, effect)
     local jpValue = target:getJobPointLevel(tpz.jp.SEIGAN_EFFECT) * 3
-    local counterRate = target:getMod(tpz.mod.ZANSHIN) / 4
-    counterRate = counterRate + target:getMod(tpz.mod.HASSO_SEIGAN_GIFT)
+    local counterRate = 10 + target:getMod(tpz.mod.HASSO_SEIGAN_GIFT)
 
     target:delMod(tpz.mod.DEF, jpValue)
     if (target:getMainJob() == tpz.job.SAM) then
