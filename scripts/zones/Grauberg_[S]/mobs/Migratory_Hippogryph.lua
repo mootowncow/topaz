15f3b865479040a3d2ed8b8f76e5d90daa05a622
Shiyo <strikinginsanity@hotmail.com>
2025-03-23 11:52:48 -0500

    (AF - DNC) AF1 NM tuning

--- scripts/zones/Grauberg_[S]/mobs/Migratory_Hippogryph.lua
+++ scripts/zones/Grauberg_[S]/mobs/Migratory_Hippogryph.lua
@@ -4,9 +4,13 @@
 -- Note: Spawned for quest "The Unfinished Waltz"
 -----------------------------------
 require("scripts/globals/quests")
+require("scripts/globals/mobs")
 -----------------------------------
 
 function onMobInitialize(mob)
+    SetGenericNMStats(mob)
+    mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
+    mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
     mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
 end
 
