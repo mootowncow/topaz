4fdd8b8e9daf406f0261fc033da309005ef05a94
Shiyo <strikinginsanity@hotmail.com>
2025-03-23 13:49:35 -0500

    (DNC) Presto changed to 5m CD

--- sql/abilities.sql
+++ sql/abilities.sql
@@ -314,7 +314,7 @@ INSERT INTO `abilities` VALUES (257,'double_shot',11,70,1,180,126,0,0,225,2000,0
 INSERT INTO `abilities` VALUES (258,'sengikori',12,25,1,90,141,0,0,226,2000,0,6,30.0,1,1,80,0,4,NULL,0);
 INSERT INTO `abilities` VALUES (259,'futae',13,50,1,60,148,0,0,227,2000,0,6,20.0,0,1,0,0,0,'ABYSSEA',0);		-- level 50, 60 second cooldown
 INSERT INTO `abilities` VALUES (260,'spirit_jump',14,77,4,60,166,100,0,228,2000,0,6,20.0,0,1,80,1218,0,NULL,0);
-INSERT INTO `abilities` VALUES (261,'presto',19,75,1,15,236,100,0,229,2000,0,6,20.0,0,1,80,0,0,NULL,0);
+INSERT INTO `abilities` VALUES (261,'presto',19,75,1,300,236,100,0,229,2000,0,6,20.0,0,1,80,0,0,NULL,0);
 INSERT INTO `abilities` VALUES (262,'divine_waltz_ii',19,55,27,20,225,102,0,34,2000,0,14,10.0,1,0,0,0,0,NULL,800);
 INSERT INTO `abilities` VALUES (263,'flourishes_iii',19,60,1,0,226,0,0,0,2000,0,14,20.0,0,0,0,0,0,NULL,0);
 INSERT INTO `abilities` VALUES (264,'climactic_flourish',19,75,1,90,226,100,0,230,2000,0,6,20.0,1,1,80,0,0,NULL,0);
