b5d2cb230e94792d3499fad34009947e458babdc
Shiyo <strikinginsanity@hotmail.com>
2025-05-05 16:26:20 -0500

    Remaining Eldieme Necropolis [S] entrance doors

--- scripts/zones/Batallia_Downs_[S]/npcs/_2cd.lua
+++ scripts/zones/Batallia_Downs_[S]/npcs/_2cd.lua
@@ -1,19 +1,19 @@
 -----------------------------------
 -- Area: Batallia Downs [S]
 -- Door: Crypt Door
--- Note: The Eldieme Necropolis [S] entrance 1
--- !gotoid 17122118
+-- Note: The Eldieme Necropolis [S] entrance 3
+-- !gotoid 17122120
 -----------------------------------
 
 function onTrigger(player, npc)
-    player:startEvent(18)
+    player:startEvent(16)
 end
 
 function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
-    if csid == 18 and option == 1 then
-        player:setPos(420, -56, -119.51, 191, 175)
+    if csid == 16 and option == 1 then
+        player:setPos(59.998, -32.000, 161.041, 66, 175)
     end
 end
--- scripts/zones/Batallia_Downs_[S]/npcs/_2cf.lua
+++ scripts/zones/Batallia_Downs_[S]/npcs/_2cf.lua
@@ -1,19 +1,19 @@
 -----------------------------------
 -- Area: Batallia Downs [S]
 -- Door: Crypt Door
--- Note: The Eldieme Necropolis [S] entrance 1
--- !gotoid 17122118
+-- Note: The Eldieme Necropolis [S] entrance 5 (4 is unaccessible on retail)
+-- !gotoid 17122122
 -----------------------------------
 
 function onTrigger(player, npc)
-    player:startEvent(18)
+    player:startEvent(14)
 end
 
 function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
-    if csid == 18 and option == 1 then
-        player:setPos(420, -56, -119.51, 191, 175)
+    if csid == 14 and option == 1 then
+        player:setPos(-160.936, -32.000, 259.974, 0.000, 175)
     end
 end
