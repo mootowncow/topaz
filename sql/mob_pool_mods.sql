ebc3def0cf00a163ca9409ba93ee55a9930391ca
Shiyo <strikinginsanity@hotmail.com>
2025-05-10 17:33:23 -0500

    Squashed commit of the following:
    commit b3f768949c385cdacea5d73e4496c9e7574c98f8
    Author: Shiyo <strikinginsanity@hotmail.com>
    Date:   Sat May 10 17:32:32 2025 -0500
    
        Update packet_system.cpp
    
    commit 609ae659129a8bd0f72a237eeef5e157492bb1e8
    Author: Shiyo <strikinginsanity@hotmail.com>
    Date:   Fri May 9 21:31:33 2025 -0500
    
        Squashed commit of the following:
    
        commit e60d270f7034fa2f5fe579c8a6d1678a302be8b0
        Author: Shiyo <strikinginsanity@hotmail.com>
        Date:   Fri May 9 21:31:18 2025 -0500
    
            Fixes
    
            Added Magic burst logic and fixed magic burst gambit
    
        commit bad05e6bd14aa770e91135357a486a3023e0d08d
        Author: Shiyo <strikinginsanity@hotmail.com>
        Date:   Fri May 9 18:14:15 2025 -0500
    
            Gessho additions
    
            Add Sange / Mikage / Warcry to Gessho
    
            Gessho now has Dual Wield flagged when calculating his attacks
    
            Increased ranged damage of NIN trusts to match 2H weapons
    
        commit 4337d61ca613ac1cc2efaa456a3a469d29e4f46d
        Author: Shiyo <strikinginsanity@hotmail.com>
        Date:   Fri May 9 17:42:17 2025 -0500
    
            Fix trusts teleporting to targets if they are CCed and target moves away
    
        commit 5b20ee75331f7371ecab2c029b4a5e9f89208be6
        Author: Shiyo <strikinginsanity@hotmail.com>
        Date:   Fri May 9 17:18:18 2025 -0500
    
            Add daken proc to trusts (Gessho)
    
    commit 8f69c120ca65f6b1ab802ea06f347577596db326
    Author: Shiyo <strikinginsanity@hotmail.com>
    Date:   Fri May 9 12:13:19 2025 -0500
    
        Squashed commit of the following:
    
        commit 7de1a97ddae31e60f5ff9d91531c1ff03d18eeef
        Author: Shiyo <strikinginsanity@hotmail.com>
        Date:   Fri May 9 12:13:06 2025 -0500
    
            Fixes

--- sql/mob_pool_mods.sql
+++ sql/mob_pool_mods.sql
@@ -45,7 +45,6 @@ INSERT INTO `mob_pool_mods` VALUES (9007,44,1,1); -- Nagi(Custom)
 INSERT INTO `mob_pool_mods` VALUES (1386,44,1,1); -- Fomor Ninja
 INSERT INTO `mob_pool_mods` VALUES (5857,44,1,1); -- Hoar-knuckled Rimberry
 INSERT INTO `mob_pool_mods` VALUES (9006,44,1,1); -- Lilisette
-INSERT INTO `mob_pool_mods` VALUES (6868,44,1,1); -- Ethniu
 
 
 INSERT INTO `mob_pool_mods` VALUES (60,370,20,0);
