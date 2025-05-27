a20e8cd31438199e623c7f94f2cef18c9ce9586c
Shiyo <strikinginsanity@hotmail.com>
2025-05-26 21:52:05 -0500

    Remove debug print

--- src/map/attack.cpp
+++ src/map/attack.cpp
@@ -837,7 +837,6 @@ void CAttack::ProcessDamage()
         if (m_attacker->objtype == TYPE_PET && m_attacker->PMaster && m_attacker->PMaster->objtype == TYPE_PC &&
             static_cast<CPetEntity*>(m_attacker)->getPetType() == PETTYPE_AUTOMATON)
         {
-            ShowDebug("Try SKill up puppet melee\n");
             puppetutils::TrySkillUP((CAutomatonEntity*)m_attacker, SKILL_AUTOMATON_MELEE, m_victim->GetMLevel());
         }
     }
