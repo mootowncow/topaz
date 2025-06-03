7c6a71e67a7dec18cf4d64c87d8b59d6a1d22239
Shiyo <strikinginsanity@hotmail.com>
2025-06-02 23:10:34 -0500

    (TP Moves) Ranged dmg type

--- scripts/globals/mobskills/hane_fubuki.lua
+++ scripts/globals/mobskills/hane_fubuki.lua
@@ -32,7 +32,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     local typeEffect = tpz.effect.POISON
 
-    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.PIERCING)
+    target:takeDamage(dmg, mob, tpz.attackType.RANGED, tpz.damageType.RANGED)
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 9, 3, 90)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
