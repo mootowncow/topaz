75ea9721ed910ffc7ae73406f22b263b96e26f52
Shiyo <strikinginsanity@hotmail.com>
2025-06-10 16:03:31 -0500

    (Mobs) no longer need these mods

--- scripts/zones/Caedarva_Mire/mobs/Heraldic_Imp.lua
+++ scripts/zones/Caedarva_Mire/mobs/Heraldic_Imp.lua
@@ -6,8 +6,6 @@ require("scripts/globals/status")
 mixins = {require("scripts/mixins/families/imp")}
 -----------------------------------
 function onMobSpawn(mob)
-	mob:setMod(tpz.mod.PIERCERES, 1000)
-    mob:setMod(tpz.mod.RANGEDRES, 1000)
 	mob:setMobMod(tpz.mobMod.MAGIC_COOL, 25)
     mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
 end
--- scripts/zones/Caedarva_Mire/mobs/Heraldic_Imp_Exp_Camp.lua
+++ scripts/zones/Caedarva_Mire/mobs/Heraldic_Imp_Exp_Camp.lua
@@ -6,8 +6,6 @@ require("scripts/globals/status")
 mixins = {require("scripts/mixins/families/imp")}
 -----------------------------------
 function onMobSpawn(mob)
-	mob:setMod(tpz.mod.PIERCERES, 1000)
-    mob:setMod(tpz.mod.RANGEDRES, 1000)
 end
 
 function onMobDeath(mob, player, isKiller, noKiller)
