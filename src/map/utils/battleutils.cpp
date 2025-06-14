9b9c415e395bf571521c1f37884588f3660d5fa6
Shiyo <strikinginsanity@hotmail.com>
2025-06-14 14:40:26 -0500

    Squashed commit of the following:
    commit b589960a36353d148046b252727fa27997920bd3
    Author: Shiyo <strikinginsanity@hotmail.com>
    Date:   Sat Jun 14 14:40:09 2025 -0500
    
        (Combat) Fix TP gain bug
    
    commit 223094b0f91d7c26e7fb8d8992c43a74f0fbe009
    Merge: f1c420273e a06ace785b
    Author: mootowncow <mootowncow@gmail.com>
    Date:   Fri Jun 13 23:14:29 2025 -0500
    
        Merge branch 'release' of https://github.com/mootowncow/topaz into release
    
    commit a06ace785be2aa80d75113502202ee4833fe5617
    Author: Shiyo <strikinginsanity@hotmail.com>
    Date:   Fri Jun 13 23:09:26 2025 -0500
    
        (PUP) Sharpshot RA tuning
    
        (PUP) Sharpshot RA tuning
    
    commit f1c420273e8733e1f67c65c05c2ea88352f5947c
    Author: Shiyo <strikinginsanity@hotmail.com>
    Date:   Fri Jun 13 23:09:26 2025 -0500
    
        (PUP) Sharpshot RA tuning

--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -3684,7 +3684,7 @@ namespace battleutils
 
             int16 baseTp = 0;
 
-            if ((slot == SLOT_RANGED || slot == SLOT_AMMO) && PAttacker->objtype == TYPE_PC || PAttacker->allegiance == ALLEGIANCE_PLAYER)
+            if (isRanged && (PAttacker->objtype == TYPE_PC || PAttacker->allegiance == ALLEGIANCE_PLAYER))
             {
                 int16 delay = PAttacker->GetRangedWeaponDelay(true);
 
