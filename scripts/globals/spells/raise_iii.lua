d2a9ea99a0cb9bf6742f03263242a451fa9e40e5
Shiyo <strikinginsanity@hotmail.com>
2025-06-17 12:45:52 -0500

    (Trusts) Raise resetting level to 1 bugfix

--- scripts/globals/spells/raise_iii.lua
+++ scripts/globals/spells/raise_iii.lua
@@ -19,6 +19,7 @@ function onSpellCast(caster, target, spell)
                 target:allowSendRaisePrompt()
                 target:addStatusEffect(tpz.effect.RERAISE, 3, 0, 3600)
             else
+                target:setLocalVar("jobLevel", target:getJobLevel(target:getMainJob()))
                 target:sendRaise(3)
             end
         end
