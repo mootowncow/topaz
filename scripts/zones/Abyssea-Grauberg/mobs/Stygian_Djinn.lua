0de9596971e183d78212e1681d2ffcb387e87058
Shiyo <strikinginsanity@hotmail.com>
2025-06-17 19:10:51 -0500

    (Mixins) Djinn mixins

---
+++ scripts/zones/Abyssea-Grauberg/mobs/Stygian_Djinn.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: Abyssea-Grauberg
+--  Mob: Stygian Djinn
+-----------------------------------
+mixins = {require("scripts/mixins/families/djinn")}
+-- -----------------------------------
+
+function onMobDeath(mob, player, isKiller, noKiller)
+end
