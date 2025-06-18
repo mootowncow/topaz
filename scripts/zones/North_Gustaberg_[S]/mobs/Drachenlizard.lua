125a140e040e59113a7e19c1a68d2df83818c975
Shiyo <strikinginsanity@hotmail.com>
2025-06-17 20:08:32 -0500

    (JP Camps) Drachen Lizard

--- scripts/zones/North_Gustaberg_[S]/mobs/Drachenlizard.lua
+++ scripts/zones/North_Gustaberg_[S]/mobs/Drachenlizard.lua
@@ -30,6 +30,7 @@ function onMobFight(mob, target)
             mob:useMobAbility(math.random(1124, 1125)) -- Heal MP or HP
         end
     end
+    mob:setMobMod(tpz.mobMod.SKILL_LIST, 174)
 end
 
 function onMobDeath(mob, player, isKiller, noKiller)
