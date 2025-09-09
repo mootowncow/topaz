6ce927cd8682c4725e8222c7ad936018b7c066a7
Shiyo <strikinginsanity@hotmail.com>
2025-09-08 21:50:29 -0500

    (WS) Steel Cyclone tuning

--- scripts/globals/weaponskills/steel_cyclone.lua
+++ scripts/globals/weaponskills/steel_cyclone.lua
@@ -21,12 +21,12 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 1
-    params.ftp100 = 1.5 params.ftp200 = 3.0 params.ftp300 = 3.5
-    params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.5 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.ftp100 = 1.5625 params.ftp200 = 1.875 params.ftp300 = 2.5
+    params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.4 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
-    params.atk100 = 1.5; params.atk200 = 1.5; params.atk300 = 1.5
+    params.atk100 = 1.75; params.atk200 = 1.75; params.atk300 = 1.75
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.ftp200 = 2.5 params.ftp300 = 4
