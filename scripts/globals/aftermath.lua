d5ae9add6ae878f7bb08835b1df99b92b29f2498
Shiyo <strikinginsanity@hotmail.com>
2025-09-16 12:58:42 -0500

    (Relic) Claustrum BP damage aftermath

--- scripts/globals/aftermath.lua
+++ scripts/globals/aftermath.lua
@@ -73,7 +73,7 @@ tpz.aftermath.effects =
     [9]  = { mods = { tpz.mod.CRITHITRATE, 15 }, duration = getTier1RelicDuration }, -- Kikoku
     [10] = { mods = { tpz.mod.TP_BONUS, 1000 }, duration = getTier1RelicDuration }, -- Amanomurakumo
     [11] = { mods = { tpz.mod.ENH_MAGIC_DURATION, 100 }, duration = getTier1RelicDuration }, -- Mjollnir
-    [12] = { mods = { tpz.mod.DOUBLE_CAST, 20 }, duration = getTier1RelicDuration }, -- Claustrum
+    [12] = { mods = { tpz.mod.DOUBLE_CAST, 20, tpz.mod.BP_DAMAGE, 25 }, duration = getTier1RelicDuration, includePets = true }, -- Claustrum
     [13] = { mods = { tpz.mod.RACC, 20 }, duration = getTier1RelicDuration }, -- Yoichinoyumi
     [14] = { mods = { tpz.mod.ENMITY, -20 }, duration = getTier1RelicDuration }, -- Annihilator
 
