80e77c1bf75d3ea93d63f143f3d6ce843ca100b3
Shiyo <strikinginsanity@hotmail.com>
2025-09-19 21:54:15 -0500

    (NMs) Mountain Worm (NM) / Phantom Worm tuning

--- scripts/zones/Kuftal_Tunnel/mobs/Phantom_Worm.lua
+++ scripts/zones/Kuftal_Tunnel/mobs/Phantom_Worm.lua
@@ -3,6 +3,7 @@
 --  Mob: Morion Worm
 -----------------------------------
 require("scripts/globals/status")
+require("scripts/globals/mobs")
 -----------------------------------
 
 function onMobInitialize(mob)
@@ -10,16 +11,12 @@ function onMobInitialize(mob)
 end
 
 function onMobSpawn(mob)
-    mob:addMod(tpz.mod.DEFP, 50) 
-    mob:addMod(tpz.mod.ATTP, 20)
-    mob:addMod(tpz.mod.ACC, 50) 
-    mob:addMod(tpz.mod.EVA, 30)
-    mob:setMod(tpz.mod.REGEN, 50)
-    mob:setMod(tpz.mod.REFRESH, 40)
+    SetGenericNMStats(mob)
     mob:setMod(tpz.mod.UFASTCAST, 50) 
     mob:addMod(tpz.mod.SPELLINTERRUPT, 50)
-    mob:addMod(tpz.mod.SLEEPRESTRAIT, 75)
-	mob:addMod(tpz.mod.SILENCERESTRAIT, 100)
+    mob:addMod(tpz.mod.EEM_LIGHT_SLEEP, 20)
+    mob:addMod(tpz.mod.EEM_DARK_SLEEP, 20)
+	mob:addMod(tpz.mod.EEM_SILENCE, 5)
 end
 
 function onMobDeath(mob, player, isKiller, noKiller)
