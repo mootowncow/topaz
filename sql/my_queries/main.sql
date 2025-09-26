85aa74afadaf0b4695c6b2116b11aaf44a9dd8e3
Shiyo <strikinginsanity@hotmail.com>
2025-09-25 21:36:56 -0500

    (Query) WOTG / Orc to always have /WAR SJ

--- sql/my_queries/main.sql
+++ sql/my_queries/main.sql
@@ -1,7 +1,12 @@
+UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 189;
+UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 190;
 UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 200;
 UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 201;
 UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 202;
 UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 270;
+UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 942;
+UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 943;
+UPDATE tpzdb . mob_pools SET sJob = 1 WHERE familyid = 944;
 UPDATE tpzdb . mob_pools SET skill_list_id = 1163 WHERE familyid = 942;
 UPDATE tpzdb . mob_pools SET skill_list_id = 1164  WHERE familyid = 943;
 UPDATE tpzdb . mob_pools SET skill_list_id = 1165 WHERE familyid = 944;
