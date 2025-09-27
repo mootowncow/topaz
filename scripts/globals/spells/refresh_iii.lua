fc78cfb6b6c4ce59606b200f40bc3e2935914bc9
Shiyo <strikinginsanity@hotmail.com>
2025-09-27 12:00:03 -0500

    Refresh 3 / Temper and animation IDs

---
+++ scripts/globals/spells/refresh_iii.lua
@@ -0,0 +1,31 @@
+-----------------------------------------
+-- Spell: Refresh III
+-- Gradually restores target party member's MP
+-- Composure increases duration 3x
+-----------------------------------------
+require("scripts/globals/magic")
+require("scripts/globals/msg")
+require("scripts/globals/status")
+-----------------------------------------
+
+function onMagicCastingCheck(caster, target, spell)
+    return 0
+end
+
+function onSpellCast(caster, target, spell)
+    local duration = calculateDuration(150, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    duration = calculateDurationForLvl(duration, 75, target:getMainLvl())
+    duration = duration + target:getMod(tpz.mod.REFRESH_DURATION)
+
+    local mp = 9 + caster:getMod(tpz.mod.ENHANCES_REFRESH)
+
+    if target:hasStatusEffect(tpz.effect.SUBLIMATION_ACTIVATED) or target:hasStatusEffect(tpz.effect.SUBLIMATION_COMPLETE) then
+        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
+        return 0
+    end
+
+    target:delStatusEffectSilent(tpz.effect.REFRESH)
+    target:addStatusEffect(tpz.effect.REFRESH, mp, 0, duration)
+
+    return tpz.effect.REFRESH
+end
