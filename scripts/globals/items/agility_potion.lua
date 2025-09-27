d8b75e136a6ace44e66388bbade0e75fc774f078
Shiyo <strikinginsanity@hotmail.com>
2025-09-27 12:41:23 -0500

    (Items) Attribute potions 1hr medicated

--- scripts/globals/items/agility_potion.lua
+++ scripts/globals/items/agility_potion.lua
@@ -17,5 +17,5 @@ function onItemUse(target)
     target:addStatusEffect(tpz.effect.AGI_BOOST, 20, 0, 30)
 	target:addStatusEffect(tpz.effect.EVASION_BOOST, 1000, 0, 30)
     target:messageBasic(tpz.msg.basic.GAINS_EFFECT_OF_STATUS, tpz.effect.EVASION_BOOST)
-    target:addStatusEffect(tpz.effect.MEDICINE, 0, 0, 900)
+    target:addStatusEffect(tpz.effect.MEDICINE, 0, 0, 3600)
 end
