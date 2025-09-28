3cdc05e408feb925c8196c56c5eca4a799101bd8
Shiyo <strikinginsanity@hotmail.com>
2025-09-27 23:48:49 -0500

    Revert "(Fishing) 10% minimum skill up rate"
    This reverts commit f20c4a34c891d69e04ff300807b21af2c9d50379.

--- src/map/utils/fishingutils.cpp
+++ src/map/utils/fishingutils.cpp
@@ -891,7 +891,7 @@ namespace fishingutils
         int skillLevelPenalty = (int)std::floor(charSkillLevel / 10);
 
         // Minimum 4% chance
-        maxChance = std::max(10, distMod + lowerLevelBonus - skillLevelPenalty);
+        maxChance = std::max(4, distMod + lowerLevelBonus - skillLevelPenalty);
 
         // Grant a bonus for catching much higher level fish
         if (levelDifference >= 21)
