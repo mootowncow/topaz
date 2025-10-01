b4afcc763568b52138a025ebb95166c667591e9c
Shiyo <strikinginsanity@hotmail.com>
2025-10-01 15:54:52 -0500

    (Sky) Faust damage tuning

--- scripts/zones/The_Shrine_of_RuAvitau/mobs/Faust.lua
+++ scripts/zones/The_Shrine_of_RuAvitau/mobs/Faust.lua
@@ -9,6 +9,7 @@ local ID = require("scripts/zones/The_Shrine_of_RuAvitau/IDs")
 -----------------------------------
 function onMobSpawn(mob)
     SetGenericNMStats(mob)
+    mob:setDamage(125)
 	mob:setMobMod(tpz.mobMod.SIGHT_RANGE, 30)
     mob:setMobMod(tpz.mobMod.GIL_MAX, 6000)
     mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
