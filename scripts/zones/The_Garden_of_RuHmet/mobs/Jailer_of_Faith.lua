ebb8103bdddd351d28fb198b5138de406fbb4993
Shiyo <strikinginsanity@hotmail.com>
2025-10-01 19:15:19 -0500

    (Sea) Jailer of Faith tuning

--- scripts/zones/The_Garden_of_RuHmet/mobs/Jailer_of_Faith.lua
+++ scripts/zones/The_Garden_of_RuHmet/mobs/Jailer_of_Faith.lua
@@ -7,10 +7,10 @@ mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 
 function onMobSpawn(mob)
-	mob:setDamage(155)
+	mob:setDamage(180)
     mob:setMod(tpz.mod.ATT, 435) -- 500
     mob:setMod(tpz.mod.DEF, 412) -- 450
-    mob:setMod(tpz.mod.EVA, 236) -- 275
+    mob:setMod(tpz.mod.EVA, 321) -- 360
     mob:setMod(tpz.mod.REFRESH, 50)
     -- Change animation to open
     mob:AnimationSub(2)
