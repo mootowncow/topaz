446f55b89ac9738fe102a480e5cc56a4f32ad834
Shiyo <strikinginsanity@hotmail.com>
2025-10-03 13:51:59 -0500

    (Assault) Ferocious Wamoura addle undispellable

--- scripts/globals/spells/addle.lua
+++ scripts/globals/spells/addle.lua
@@ -31,6 +31,9 @@ function onSpellCast(caster, target, spell)
     local duration = 180 * resist
 
     TryApplyEffect(caster, target, spell, params.effect, power, 0, duration, resist, 0.5, power)
+    if (caster:getName() == 'Ferocious_Wamoura') then
+        target:setEffectUndispellable(tpz.effect.ADDLE)
+    end
 
     return params.effect
 end
