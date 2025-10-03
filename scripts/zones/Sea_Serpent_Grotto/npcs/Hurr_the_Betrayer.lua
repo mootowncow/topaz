d50c6b3ca1968fc8cb6b4a76c130c400348d531e
Shiyo <strikinginsanity@hotmail.com>
2025-10-03 13:51:27 -0500

    (Quests) Sahagin key quest made retail accurate
    Now always asks for 3 Mythril Beastcoins + 1 Norg shell on first Key request
    
    Now always asks for Gold Beastcoin + Norg shell on repeats

--- scripts/zones/Sea_Serpent_Grotto/npcs/Hurr_the_Betrayer.lua
+++ scripts/zones/Sea_Serpent_Grotto/npcs/Hurr_the_Betrayer.lua
@@ -5,37 +5,33 @@
 -- !pos 305.882 26.768 234.279 176
 -----------------------------------
 local ID = require("scripts/zones/Sea_Serpent_Grotto/IDs")
+require("scripts/globals/quests")
 -----------------------------------
 
 function onTrade(player, npc, trade)
     if player:getCharVar("SahaginKeyItems") == 1 then -- If player was told to use 3 Mythril Beastcoins
-        if npcUtil.tradeHas(trade, {{749, 3}, {1135, 1}}) then
+        if npcUtil.tradeHasExactly(trade, {{tpz.items.MYTHRIL_BEASTCOIN, 3}, {tpz.items.NORG_SHELL, 1}}) then
             player:startEvent(107)
         end
     elseif (player:getCharVar("SahaginKeyItems") == 2) then -- If player was told to use a Gold Beastcoin
-        if npcUtil.tradeHas(trade, {748, 1135}) then
+        if npcUtil.tradeHasExactly(trade, {tpz.items.GOLD_BEASTCOIN, tpz.items.NORG_SHELL}) then
             player:startEvent(107)
         end
     end
 end
 
 function onTrigger(player, npc)
-    if player:getCharVar("SahaginKeyProgress") == 2 and not player:hasItem(1197) then -- If player has never before finished the quest
+    if player:getCharVar("SahaginKeyProgress") == 2 and not player:hasItem(tpz.items.SAHAGIN_KEY) then -- If player has never before finished the quest
         player:startEvent(105)
         player:setCharVar("SahaginKeyItems", 1)
-    elseif player:getCharVar("SahaginKeyProgress") == 3 and player:getCharVar("SahaginKeyItems") == 0 and not player:hasItem(1197) then
-        if math.random(2) == 1 then
-            player:startEvent(105) -- Requires 3 Mythril Beastcoins and a Norg Shell
-            player:setCharVar("SahaginKeyItems", 1)
-        else
-            player:startEvent(106) -- Requires Gold Beastcoin and a Norg Shell
-            player:setCharVar("SahaginKeyItems", 2)
-        end
+    elseif player:getCharVar("SahaginKeyProgress") == 3 and player:getCharVar("SahaginKeyItems") == 0 and not player:hasItem(tpz.items.SAHAGIN_KEY) then
+        player:startEvent(106) -- Requires Gold Beastcoin and a Norg Shell
+        player:setCharVar("SahaginKeyItems", 2)
     elseif player:getCharVar("SahaginKeyProgress") == 3 and player:getCharVar("SahaginKeyItems") == 1 then
         player:startEvent(105) -- If player was told to use 3 Mythril Beastcoins
     elseif player:getCharVar("SahaginKeyProgress") == 3 and player:getCharVar("SahaginKeyItems") == 2 then
         player:startEvent(106) -- If player was told to use a Gold Beastcoin
-    elseif player:getCharVar("SahaginKeyProgress") == 2 and player:hasItem(1197) then
+    elseif player:getCharVar("SahaginKeyProgress") == 2 and player:hasItem(tpz.items.SAHAGIN_KEY) then
         player:startEvent(104) -- Doesn't offer the key again if the player has one
     else
         player:startEvent(104) -- Doesn't offer the key if the player hasn't spoken to the first 2 NPCs
@@ -46,12 +42,12 @@ function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
-    if csid == 107 and player:getCharVar("SahaginKeyProgress") == 2 and npcUtil.giveItem(player, 1197) then
-        player:confirmTrade()
+    if csid == 107 and player:getCharVar("SahaginKeyProgress") == 2 and npcUtil.giveItem(player, tpz.items.SAHAGIN_KEY) then
+        player:tradeComplete()
         player:setCharVar("SahaginKeyProgress", 3) -- Mark the quest progress
         player:setCharVar("SahaginKeyItems", 0)
-    elseif csid == 107 and player:getCharVar("SahaginKeyProgress") == 3 and npcUtil.giveItem(player, 1197) then
-        player:confirmTrade()
+    elseif csid == 107 and player:getCharVar("SahaginKeyProgress") == 3 and npcUtil.giveItem(player, tpz.items.SAHAGIN_KEY) then
+        player:tradeComplete()
         player:setCharVar("SahaginKeyItems", 0)
     end
 end
