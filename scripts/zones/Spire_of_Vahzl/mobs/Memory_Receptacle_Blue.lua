9c182aa8c4e04301c97c6e7aa20891335c4fc799
Shiyo <strikinginsanity@hotmail.com>
2025-10-06 20:38:18 -0500

    (ENM) Pulling the Plug set speed to 0 on spawn

--- scripts/zones/Spire_of_Vahzl/mobs/Memory_Receptacle_Blue.lua
+++ scripts/zones/Spire_of_Vahzl/mobs/Memory_Receptacle_Blue.lua
@@ -14,6 +14,7 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.DEFP, 20) 
     mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
     mob:setLocalVar("path", vahzl.PATH_TOP)
+    mob:speed(0)
 end
 
 function onMobFight(mob, target)
