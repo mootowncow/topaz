93c85f88e0db3e5e838e342e021cbb3304f76a79
Shiyo <strikinginsanity@hotmail.com>
2025-10-09 14:44:31 -0500

    (GM Commands)

--- scripts/commands/healall.lua
+++ scripts/commands/healall.lua
@@ -9,26 +9,15 @@ cmdprops =
 }
 
 function onTrigger(player)
-    local zonePlayers = player:getZone():getPlayers()
-    for _, zonePlayer in pairs(zonePlayers) do
-        -- delete status effect
-        for v = 1,31,1 do
-            zonePlayer:delStatusEffectSilent(v)
+    local nearbyEntities = player:getNearbyEntities(100)
+    if (nearbyEntities ~= nil) then
+        for _, nearbyPlayers in pairs(nearbyEntities) do
+            if (nearbyPlayers:isPC() or nearbyPlayers:isTrust()) then
+                nearbyPlayers:removeAllNegativeEffects()
+                nearbyPlayers:setHPP(100)
+                nearbyPlayers:setMPP(100)
+            end
         end
-        for v = 128,142,1 do
-            zonePlayer:delStatusEffectSilent(v)
-        end
-        for v = 144,149,1 do
-            zonePlayer:delStatusEffectSilent(v)
-        end
-        for v = 167,168,1 do
-            zonePlayer:delStatusEffectSilent(v)
-        end
-        for v = 192,194,1 do
-            zonePlayer:delStatusEffectSilent(v)
-        end
-        zonePlayer:setHP(3000)
-        zonePlayer:setMP(3000)
-        zonePlayer:PrintToPlayer( string.format( "Successfully healed everyone's HP and MP to full and removed all negative status effects.") )
+        player:PrintToPlayer( string.format( "Successfully healed everyone's HP and MP to full and removed all negative status effects.") )
     end
 end
