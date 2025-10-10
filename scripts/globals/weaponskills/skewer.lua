480e31c98137958c99bb1521cd8e5f55bd4dc160
Shiyo <strikinginsanity@hotmail.com>
2025-10-10 18:16:00 -0500

    (WS) Tuning

--- scripts/globals/weaponskills/skewer.lua
+++ scripts/globals/weaponskills/skewer.lua
@@ -21,12 +21,12 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 3
     params.ftp100 = 1.25 params.ftp200 = 1.25 params.ftp300 = 1.25
-    params.str_wsc = 0.35 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.15 params.crit200 = 0.25 params.crit300 = 0.40
     params.canCrit = true
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
-    params.atk100 = 1.25; params.atk200 = 1.25; params.atk300 = 1.25
-    params.multiHitfTP = true
+    params.atk100 = 1.0; params.atk200 = 1.0; params.atk300 = 1.0
+    params.multiHitfTP = false
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.str_wsc = 0.5
