fe9c8ce31ac02f92f8bd0f8a73f5d78a3cef53f0
Shiyo <strikinginsanity@hotmail.com>
2025-10-15 12:03:56 -0500

    (WAR) Can't retaliate vs perfect dodge and fly high

--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -1387,8 +1387,12 @@ namespace battleutils
         }
 
         // Handle Retaliation
-        if (PDefender->StatusEffectContainer->HasStatusEffect(EFFECT_RETALIATION) && PDefender->PAI->IsEngaged() &&
-            facing(PDefender->loc.p, PAttacker->loc.p, 64) && !PDefender->StatusEffectContainer->HasPreventActionEffect(false))
+        if (PDefender->StatusEffectContainer->HasStatusEffect(EFFECT_RETALIATION) &&
+            PDefender->PAI->IsEngaged() &&
+            facing(PDefender->loc.p, PAttacker->loc.p, 64) &&
+            !PDefender->StatusEffectContainer->HasPreventActionEffect(false) &&
+            !PAttacker->StatusEffectContainer->HasStatusEffect(EFFECT_PERFECT_DODGE) &&
+            !PAttacker->StatusEffectContainer->HasStatusEffect(EFFECT_FLY_HIGH))
         {
             // Calculate the base retaliation chance
             auto chance = 4 + battleutils::GetHitRate(PDefender, PAttacker);
