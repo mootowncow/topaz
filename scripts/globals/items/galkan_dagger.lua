f9a7f87d29614d912f91a446f4503579526ee231
Shiyo <strikinginsanity@hotmail.com>
2025-10-16 18:26:00 -0500

    (Items - Add Effect) MACC bonus and correct skill

--- scripts/globals/items/galkan_dagger.lua
+++ scripts/globals/items/galkan_dagger.lua
@@ -11,7 +11,8 @@ require("scripts/globals/msg")
 function onAdditionalEffect(player, target, damage)
     local chance = CalculateAdditionalEffectChance(player, 100)
     local power = math.random(8, 10)
-    local dmg = doAdditionalEffectDamage(player, target, chance, power, nil, false, 0, tpz.magic.ele.EARTH, 0)
+    local bonus = 255
+    local dmg = doAdditionalEffectDamage(player, target, chance, power, nil, false, 0, tpz.magic.ele.EARTH, tpz.skill.DAGGER, bonus)
 
     if dmg == 0 then
         return 0, 0, 0
