877e1022695c59b376e28ca28414cc70181eabca
Shiyo <strikinginsanity@hotmail.com>
2025-10-22 16:10:08 -0500

    (Lockstyle) Fix fishing rods making characters sing
    If you lockstyle ammo/ranged to "keep same piece", equip a fishing rod, then equip ONLY an ammo (i.e. pebble) and throw it, your character sings

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -2343,6 +2343,7 @@ namespace charutils
                 break;
             }
             case SLOT_RANGED:
+            case SLOT_AMMO:
             {
                 if (hasValidStyle(PChar, PItem, appearance))
                 {
