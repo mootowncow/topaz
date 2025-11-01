37cf746be52549768d47e239da9e373f92329951
Shiyo <strikinginsanity@hotmail.com>
2025-10-31 19:57:19 -0500

    Unneeded code, can't counter during footwork

--- src/map/entities/battleentity.cpp
+++ src/map/entities/battleentity.cpp
@@ -2456,12 +2456,6 @@ bool CBattleEntity::OnAttack(CAttackState& state, action_t& action)
                         // Add extra damage multipliers
                         damage = battleutils::HandleExtraDamageMultipliers(PTarget, damage);
 
-                        // Reduce counter damage if footwork is active to 50% for balancing reasons
-                        if (PTarget->StatusEffectContainer->HasStatusEffect(EFFECT_FOOTWORK))
-                        {
-                            damage *= 0.50;
-                        }
-
                         actionTarget.spikesParam = battleutils::TakePhysicalDamage(PTarget, this, attack.GetAttackType(), damage, false, SLOT_MAIN, 1, nullptr, true, false, true);
                         actionTarget.spikesMessage = 33;
 
