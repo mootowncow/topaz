86206c3ae247302c502612fbd5f14711e9d7af07
Shiyo <strikinginsanity@hotmail.com>
2025-10-31 19:21:50 -0500

    (MNK) Footwork and Dragon / Tornado kick tuning

--- scripts/globals/effects/footwork.lua
+++ scripts/globals/effects/footwork.lua
@@ -8,9 +8,10 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     local jpLevel = target:getJobPointLevel(tpz.jp.FOOTWORK_EFFECT)
+    local attpMod = target:getMod(tpz.mod.FOOTWORK_ATT_BONUS)
 
-	target:addMod(tpz.mod.MAIN_DMG_RATING, 25 + jpLevel)
     target:addMod(tpz.mod.KICK_DMG, effect:getPower() + jpLevel)
+	target:addMod(tpz.mod.ATTP, 10 + attpMod)
 end
 
 function onEffectTick(target, effect)
@@ -18,7 +19,8 @@ end
 
 function onEffectLose(target, effect)
     local jpLevel = target:getJobPointLevel(tpz.jp.FOOTWORK_EFFECT)
+    local attpMod = target:getMod(tpz.mod.FOOTWORK_ATT_BONUS)
 
-    target:delMod(tpz.mod.MAIN_DMG_RATING, 25 + jpLevel)
     target:delMod(tpz.mod.KICK_DMG, effect:getPower() + jpLevel)
+	target:delMod(tpz.mod.ATTP, 10 + attpMod)
 end
--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -495,14 +495,6 @@ function doPhysicalWeaponskill(attacker, target, wsID, wsParams, tp, action, pri
     calcParams = calculateRawWSDmg(attacker, target, wsID, tp, action, wsParams, calcParams, false)
     local finaldmg = calcParams.finalDmg
 
-    -- Delete statuses that may have been spent by the WS
-    attacker:delStatusEffectsByFlag(tpz.effectFlag.DETECTABLE)
-    attacker:delStatusEffectsByFlag(tpz.effectFlag.ATTACK)
-    attacker:delStatusEffectsByFlag(tpz.effectFlag.PHYS_ATTACK)
-    attacker:delStatusEffectSilent(tpz.effect.SNEAK_ATTACK)
-    attacker:delStatusEffectSilent(tpz.effect.BUILDING_FLOURISH)
-	attacker:delStatusEffectSilent(tpz.effect.CONSUME_MANA)
-
     -- Handle Null
     finaldmg = utils.CheckForNull(attacker, target, tpz.attackType.PHYSICAL, tpz.magic.ele.NONE, finaldmg)
 
@@ -573,10 +565,26 @@ function doPhysicalWeaponskill(attacker, target, wsID, wsParams, tp, action, pri
         end
     end
 
+    -- Handle Footwork 
+	if attacker:hasStatusEffect(tpz.effect.FOOTWORK) then
+        printf("Foowork is active")
+        if (wsID == tpz.weaponskill.DRAGON_KICK) or (wsID == tpz.weaponskill.TORNADO_KICK) then
+	        finaldmg = math.floor(finaldmg * 1.5)
+	    end
+    end
+
     -- Handle Scarlet Delirium
     finaldmg = utils.ScarletDeliriumBonus(attacker, finaldmg)
 
     finaldmg = utils.HandleExtraDamageMultipliers(attacker, finaldmg)
+
+    -- Delete statuses that may have been spent by the WS
+    attacker:delStatusEffectsByFlag(tpz.effectFlag.DETECTABLE)
+    attacker:delStatusEffectsByFlag(tpz.effectFlag.ATTACK)
+    attacker:delStatusEffectsByFlag(tpz.effectFlag.PHYS_ATTACK)
+    attacker:delStatusEffectSilent(tpz.effect.SNEAK_ATTACK)
+    attacker:delStatusEffectSilent(tpz.effect.BUILDING_FLOURISH)
+	attacker:delStatusEffectSilent(tpz.effect.CONSUME_MANA)
 
     finaldmg = finaldmg * WEAPON_SKILL_POWER -- Add server bonus
     calcParams.finalDmg = finaldmg
