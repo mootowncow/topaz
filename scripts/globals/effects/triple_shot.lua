3ed0b76b8b61e4a722f2b8121a4c7288216c4685
Shiyo <strikinginsanity@hotmail.com>
2025-11-03 11:41:57 -0600

    (COR) Triple shot duration 90s ->3m

--- scripts/globals/effects/triple_shot.lua
+++ scripts/globals/effects/triple_shot.lua
@@ -7,7 +7,6 @@ function onEffectGain(target, effect)
     local jpValue = target:getJobPointLevel(tpz.jp.TRIPLE_SHOT_EFFECT)
 
     target:addMod(tpz.mod.TRIPLE_SHOT_RATE, effect:getPower() + jpValue)
-    target:addMod(tpz.mod.RACC, 30)
 end
 
 function onEffectTick(target, effect)
@@ -17,5 +16,4 @@ function onEffectLose(target, effect)
     local jpValue = target:getJobPointLevel(tpz.jp.TRIPLE_SHOT_EFFECT)
 
     target:delMod(tpz.mod.TRIPLE_SHOT_RATE, effect:getPower() + jpValue)
-    target:delMod(tpz.mod.RACC, 30)
 end
