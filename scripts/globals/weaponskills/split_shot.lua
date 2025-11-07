de7182f602fe154bf6d588fa9c9398bb49d0503d
Shiyo <strikinginsanity@hotmail.com>
2025-11-06 20:25:52 -0600

    (WS) TUning

--- scripts/globals/weaponskills/split_shot.lua
+++ scripts/globals/weaponskills/split_shot.lua
@@ -31,10 +31,9 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
         params.agi_wsc = 0.7
     end
 
-    -- Defense ignored is 0%, 35%, 50% as per wiki.bluegartr.com
     params.ignoresDef = true
     params.ignored100 = 0.2
-    params.ignored200 = 0.5
+    params.ignored200 = 0.35
     params.ignored300 = 0.5
 
     local damage, criticalHit, tpHits, extraHits = doRangedWeaponskill(player, target, wsID, params, tp, action, primary)
