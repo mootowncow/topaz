88f3502dc8f68fcaabb2fa3c3659b9a516df2d9a
Shiyo <strikinginsanity@hotmail.com>
2025-11-09 18:41:05 -0600

    (RDM) Flurry effect fix

--- scripts/globals/spells/flurry.lua
+++ scripts/globals/spells/flurry.lua
@@ -14,7 +14,7 @@ function onSpellCast(caster, target, spell)
     local duration = calculateDuration(180, spell:getSkillType(), spell:getSpellGroup(), caster, target)
     duration = calculateDurationForLvl(duration, 48, target:getMainLvl())
 
-    if target:addStatusEffect(tpz.effect.FLURRY, 15, 0, duration) then
+    if target:addStatusEffect(tpz.effect.FLURRY_II, 15, 0, duration) then
         spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB_IS)
     else
         spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
