d8f716f6c2d18d2e986e01563c93511ed61eff88
Shiyo <strikinginsanity@hotmail.com>
2025-11-13 12:38:45 -0600

    (Shops) Remove OOE items

--- scripts/zones/RuLude_Gardens/npcs/Macchi_Gazlitah.lua
+++ scripts/zones/RuLude_Gardens/npcs/Macchi_Gazlitah.lua
@@ -14,23 +14,21 @@ end
 function onTrigger(player, npc)
     local stock =
     {
-        5703,    100,    -- Uleguerand Milk
+        5703,    334,    -- Uleguerand Milk
         5684,    250,    -- Chalaimbille
         17905,   100,    -- Wormy Broth
-        --[[
         5686,    800,    -- Cheese Sandwich
         5729,   3360,    -- Bavarois
         5718,   1300,    -- Cream Puff
         461,    5000,    -- Buffalo Milk Case
-        5152,   1280,    -- Buffalo Meat
-        4722,  31878,    -- Enfire II
-        4723,  30492,    -- Enblizzard II
-        4724,  27968,    -- Enaero II
-        4725,  26112,    -- Enstone II
-        4726,  25600,    -- Enthunder II
-        4727,  33000,    -- Enwater II
-        4850, 150000,    -- Refresh II
-        ]]--
+        5152,   1666,    -- Buffalo Meat
+        -- 4722,  31878,    -- Enfire II
+        -- 4723,  30492,    -- Enblizzard II
+        -- 4724,  27968,    -- Enaero II
+        -- 4725,  26112,    -- Enstone II
+        -- 4726,  25600,    -- Enthunder II
+        -- 4727,  33000,    -- Enwater II
+        -- 4850, 150000,    -- Refresh II
     }
 
     player:showText(npc, ID.text.MACCHI_GAZLITAH_SHOP_DIALOG1)
