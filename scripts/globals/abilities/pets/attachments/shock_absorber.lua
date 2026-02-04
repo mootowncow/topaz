9fdf9aeed60a5db64b45f86aaf65a762b16218e7
Shiyo <strikinginsanity@hotmail.com>
2026-02-03 18:04:26 -0600

    (PUP) Shock Absorber use logic
    Now uses as soon as a targets in melee range

--- scripts/globals/abilities/pets/attachments/shock_absorber.lua
+++ scripts/globals/abilities/pets/attachments/shock_absorber.lua
@@ -5,11 +5,13 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEquip(pet)
+    -- TODO: target:checkDistance() <= automaton:getMeleeRange() and make sure functions are correct, add to other shock absorbers too
+    -- TODO: <7 might be too low? Also edit in disruptor or anywhere else it's used then
     pet:setLocalVar("shockabsorber", pet:getLocalVar("shockabsorber") + 1)
     pet:addListener("AUTOMATON_ATTACHMENT_CHECK", "ATTACHMENT_SHOCK_ABSORBER", function(automaton, target)
         local master = automaton:getMaster()
         if not automaton:hasRecast(tpz.recast.ABILITY, 1946) and master and master:countEffect(tpz.effect.EARTH_MANEUVER) > 0  and
-        not automaton:hasStatusEffect(tpz.effect.STONESKIN) and automaton:getHPP() <= 99 then
+        not automaton:hasStatusEffect(tpz.effect.STONESKIN) and (automaton:checkDistance(target) - target:getModelSize()) < 7 then
             automaton:useMobAbility(1946, automaton)
         end
     end)
