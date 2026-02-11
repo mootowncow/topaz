ea61ed7cb609e8b85a1f1151b2feca9a90b349d7
Shiyo <strikinginsanity@hotmail.com>
2026-02-11 16:00:00 -0600

    Reapply "(SERVER) Crash fix"
    This reverts commit 37e5444da1cbf790f3761ce6372be32710f9fbe6.

--- src/map/item_container.cpp
+++ src/map/item_container.cpp
@@ -43,10 +43,11 @@ CItemContainer::CItemContainer(uint16 LocationID)
 
 CItemContainer::~CItemContainer()
 {
-	for (uint8 SlotID = 0; SlotID <= m_size; ++SlotID)
-	{
+    for (uint8 SlotID = 0; SlotID < m_size; ++SlotID)
+    {
         delete m_ItemList[SlotID];
-	}
+        m_ItemList[SlotID] = nullptr;
+    }
 }
 
 uint16 CItemContainer::GetID()
