e59361c695777a1c495ce513476c3b24145d0d7e
Shiyo <strikinginsanity@hotmail.com>
2026-02-15 13:49:05 -0600

    (Effects) Remove signet PDT/Evasion bonus

--- scripts/globals/effects/signet.lua
+++ scripts/globals/effects/signet.lua
@@ -8,14 +8,11 @@
 --   Increased Healing HP
 --   No TP loss while resting
 --   Bonus experience earned in smaller parties
---   Increased defense and evasion against attacks from your auto-attack target when even match or lower
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addLatent(tpz.latent.SIGNET_BONUS, 0, tpz.mod.DMGPHYS_II, 15)
-    target:addLatent(tpz.latent.SIGNET_BONUS, 0, tpz.mod.EVA, 50)
     -- Gain +15% more EXP in Conquest zones
     target:addLatent(tpz.latent.SIGNET_EXP_BONUS, 0, tpz.mod.EXP_BONUS, 15) 
 end
@@ -24,7 +21,5 @@ function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delLatent(tpz.latent.SIGNET_BONUS, 0, tpz.mod.DMGPHYS_II, 15)
-    target:delLatent(tpz.latent.SIGNET_BONUS, 0, tpz.mod.EVA, 50)
     target:delLatent(tpz.latent.SIGNET_EXP_BONUS, 0, tpz.mod.EXP_BONUS, 15)
 end
