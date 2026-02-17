a0591f35f544cc1bcc435803344dae736c81819b
Shiyo <strikinginsanity@hotmail.com>
2026-02-17 14:29:09 -0600

    (Mobs - TP Moves) Corrected Thunder Breaks stun duration

--- scripts/globals/mobskills/thunder_break.lua
+++ scripts/globals/mobskills/thunder_break.lua
@@ -15,11 +15,19 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = tpz.effect.STUN
     local dmgmod = 1.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.THUNDER, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING)
-    MobStatusEffectMove(mob, target, typeEffect, 1, 0, 8)
+
+    local effect = tpz.effect.STUN
+    local power = 1
+    local tick = 0
+    local duration = 10
+    local tier = 1
+    local subId = 1
+    local subPower = 1
+    local isGaze = false
+    MobStatusEffectMoveSub(mob, target, effect, power, tick, duration, subId, subPower, tier, isGaze)
     return dmg
 end
