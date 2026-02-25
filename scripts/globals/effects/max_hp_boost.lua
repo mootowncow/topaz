28e21c5babdc87546124decf82028eb5b48f97a6
Shiyo <strikinginsanity@hotmail.com>
2026-02-25 14:08:38 -0600

    (Effects) Max HP Boost / Yonin now keep targets HP at the same percent after adding the increased HP

--- scripts/globals/effects/max_hp_boost.lua
+++ scripts/globals/effects/max_hp_boost.lua
@@ -7,7 +7,13 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
+    local currentHpPercent = target:getHPP()
     target:addMod(tpz.mod.HPP, effect:getPower())
+
+    -- Set HP percent back to what it was before the HP boost
+    target:queue(0, function(target)
+        target:setHPP(currentHpPercent)
+    end)
 end
 
 function onEffectTick(target, effect)
--- scripts/globals/effects/yonin.lua
+++ scripts/globals/effects/yonin.lua
@@ -8,10 +8,17 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     local hpBoost = target:getMainLvl() * 3
+    local currentHpPercent = target:getHPP()
+    printf("currentHpPercent %d", currentHpPercent)
 
     target:addMod(tpz.mod.NINJA_TOOL, effect:getPower())
     target:addMod(tpz.mod.ENMITY, effect:getPower())
     target:addMod(tpz.mod.HP, hpBoost)
+
+    -- Set HP percent back to what it was before the HP boost
+    target:queue(0, function(target)
+        target:setHPP(currentHpPercent)
+    end)
 end
 
 function onEffectTick(target, effect)
