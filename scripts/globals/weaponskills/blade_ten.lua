5da76a33d89c2cb784ea286f3d010492624ee861
Shiyo <strikinginsanity@hotmail.com>
2026-02-25 11:12:17 -0600

    (WS) Tuning

--- scripts/globals/weaponskills/blade_ten.lua
+++ scripts/globals/weaponskills/blade_ten.lua
@@ -27,9 +27,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
 	params.ignoresDef = true
-    params.ignored100 = 0.33
-    params.ignored200 = 0.33
-    params.ignored300 = 0.33
+    params.ignored100 = 0.25 params.ignored200 = 0.25 params.ignored300 = 0.25
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.ftp100 = 4.5 params.ftp200 = 11.5 params.ftp300 = 15.5
