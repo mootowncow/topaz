6f8dcc4a883e4198b0faf8d12e73fa6e290b4227
Shiyo <strikinginsanity@hotmail.com>
2026-03-03 12:29:00 -0600

    (Items) Implement "Melt" weapons

--- scripts/globals/items/melt_katana.lua
+++ scripts/globals/items/melt_katana.lua
@@ -7,7 +7,33 @@
 require("scripts/globals/status")
 require("scripts/globals/magic")
 require("scripts/globals/msg")
+require("scripts/globals/items")
 -----------------------------------
+function onItemCheck(itemUser)
+    local target = itemUser:getCursorTarget()
+    if not target or not target:isMob() then
+        return tpz.msg.basic.ITEM_UNABLE_TO_USE
+    end
+
+    return 0
+end
+
+function onItemUse(target, item, itemUser)
+    local chance = CalculateAdditionalEffectChance(itemUser, 100)
+    local effect = tpz.effect.DEFENSE_DOWN
+    local power = item:getParam()
+    local tick = 0
+    local duration = 300
+    local subpower = 0
+    local tier = 1
+    local element = tpz.magic.ele.WIND
+    local skill = tpz.skill.DAGGER
+    local bonus = 255
+
+    local target = itemUser:getCursorTarget()
+
+    return TryApplyAdditionalEffect(itemUser, target, effect, element, power, tick, duration, subpower, tier, chance, skill, bonus)
+end
 
 function onAdditionalEffect(player, target, damage)
     local chance = CalculateAdditionalEffectChance(player, 10)
