d4eb51432833ef81a1ba490b716d6fd87bc2791e
Shiyo <strikinginsanity@hotmail.com>
2026-03-12 17:01:14 -0500

    (Trusts) Kupipi tuning

--- scripts/globals/trust.lua
+++ scripts/globals/trust.lua
@@ -166,12 +166,13 @@ local modByMobName =
 
     ['kupipi'] = function(mob)
         mob:addMod(tpz.mod.HPP, 20)
+        if mob:getMainLvl() >= 68 then
+            mob:addMod(tpz.mod.CURE_POTENCY, 10)
+        end
         if mob:getMainLvl() >= 75 then
-            mob:addMod(tpz.mod.DMGAOE, -33)
+            mob:addMod(tpz.mod.CURE_CAST_TIME, 35)
         end
         AddRefreshGear(mob)
-        mob:addMod(tpz.mod.CURE_CAST_TIME, 35)
-        mob:addMod(tpz.mod.CURE_POTENCY, 10)
         AddHealerGear(mob)
         AddArtifactGear(mob)
     end,
