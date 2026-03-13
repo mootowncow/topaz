51e08523cda62903661db5ff259c90aabdd7de4c
Shiyo <strikinginsanity@hotmail.com>
2026-03-12 22:25:38 -0500

    (NMs) Misc tuning based on retail

--- scripts/zones/Castle_Oztroja/mobs/Saa_Doyi_the_Fervid.lua
+++ scripts/zones/Castle_Oztroja/mobs/Saa_Doyi_the_Fervid.lua
@@ -2,11 +2,14 @@
 -- Area: Castle Oztroja (151)
 --   NM: Saa Doyi the Fervid
 -----------------------------------
+require("scripts/globals/mobs")
 require("scripts/globals/hunts")
 mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller, noKiller)
+    SetGenericNMStats(mob)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 100)
     tpz.hunts.checkHunt(mob, player, 304)
 end
 
