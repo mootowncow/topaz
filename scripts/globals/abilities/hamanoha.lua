db7f4b9f2dbfdf4c1e346db4c1d71c4e144d04a0
Shiyo <strikinginsanity@hotmail.com>
2026-03-14 18:23:52 -0500

    (Temp) Make Hamanoha undispellable

--- scripts/globals/abilities/hamanoha.lua
+++ scripts/globals/abilities/hamanoha.lua
@@ -20,6 +20,9 @@ function onUseAbility(player, target, ability)
         local demon = target:getSystem() == 9
 	    if demon then
             target:addStatusEffect(tpz.effect.HAMANOHA, 12, 0, 180 + jpValue)
+            if target:hasStatusEffect(tpz.effect.HAMANOHA) then
+                target:setEffectUndispellable(tpz.effect.HAMANOHA)
+            end
         end
     end
 end
