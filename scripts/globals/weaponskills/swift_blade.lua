f323ebef058454954142904decd55fdaaf3e6d43
Shiyo <strikinginsanity@hotmail.com>
2026-03-16 18:33:18 -0500

    (WS) Tuning

--- scripts/globals/weaponskills/swift_blade.lua
+++ scripts/globals/weaponskills/swift_blade.lua
@@ -21,11 +21,11 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 3
     params.ftp100 = 1.5 params.ftp200 = 1.5 params.ftp300 = 1.5
-    params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.4 params.chr_wsc = 0.0
+    params.str_wsc = 0.3 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.3 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.accuracyVariesWithTP = true
-    params.atk100 = 1.5; params.atk200 = 1.5; params.atk300 = 1.5
+    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
     params.multiHitfTP = true
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
