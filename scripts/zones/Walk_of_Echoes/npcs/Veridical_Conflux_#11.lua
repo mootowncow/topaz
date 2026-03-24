c7a6113faa1cd7b07ed5cc13a30ac5225d3eba61
Shiyo <strikinginsanity@hotmail.com>
2026-03-24 14:53:47 -0500

    Verdical Conflux #1-15 files

---
+++ scripts/zones/Walk_of_Echoes/npcs/Veridical_Conflux_#11.lua
@@ -0,0 +1,22 @@
+-----------------------------------
+-- Area: Walk Of Echoes
+--  NPC: Verdical Conflux #11
+-- !gotoid 17523238
+-----------------------------------
+require("scripts/globals/walk_of_echoes")
+-----------------------------------
+
+function onTrade(player, npc, trade)
+end
+
+function onTrigger(player, npc)
+    tpz.woe.verdicalConflux.onTrigger(player, npc)
+end
+
+function onEventUpdate(player, csid, option)
+    tpz.woe.verdicalConflux.onEventUpdate(player, csid, option)
+end
+
+function onEventFinish(player, csid, option)
+    tpz.woe.verdicalConflux.onEventFinish(player, csid, option)
+end
