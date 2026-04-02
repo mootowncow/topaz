92a2e048645bff54428e2e8464d1e223c16530fa
Shiyo <strikinginsanity@hotmail.com>
2026-04-01 20:02:45 -0500

    Create radial_arcana.lua

---
+++ scripts/globals/abilities/radial_arcana.lua
@@ -0,0 +1,12 @@
+-- NOTE: Radial Arcana is based on Luopan HP (and is magically modified by lightsday),
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+-----------------------------------
+
+function onAbilityCheck(player, target, ability)
+    return 0, 0
+end
+
+function onUseAbility(player, target, ability)
+end
