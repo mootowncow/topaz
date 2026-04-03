5675177c0fa572686b71b5a45ac6fd555d1a3fa5
Shiyo <strikinginsanity@hotmail.com>
2026-04-03 13:13:18 -0500

    Update Geush_Urvan.lua

--- scripts/zones/Uleguerand_Range/mobs/Geush_Urvan.lua
+++ scripts/zones/Uleguerand_Range/mobs/Geush_Urvan.lua
@@ -8,8 +8,9 @@ require("scripts/globals/mobs")
 ------------------------------
 
 function onMobSpawn(mob)
+    mob:setDamage(750)
+    mob:addMod(tpz.mod.ATTP, 100)
     mob:addMod(tpz.mod.DEFP, 25) 
-    mob:addMod(tpz.mod.ATTP, 10)
     mob:addMod(tpz.mod.ACC, 15) 
     mob:addMod(tpz.mod.EVA, 15)
     mob:setMod(tpz.mod.REFRESH, 40)
