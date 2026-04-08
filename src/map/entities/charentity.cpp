1296fe3cc972649c4483210b22d6cf44c13668c0
Shiyo <strikinginsanity@hotmail.com>
2026-04-08 14:14:27 -0500

    Add powder boots to global lock out timer

--- src/map/entities/charentity.cpp
+++ src/map/entities/charentity.cpp
@@ -2641,7 +2641,7 @@ void CCharEntity::OnRaise()
 
 void CCharEntity::OnItemFinish(CItemState& state, action_t& action)
 {
-    static const std::vector<uint16> lockOutTimerItems = { 4254, 4144, 4145 }; // Megalixir, Elixir, Hi-Elixir
+    static const std::vector<uint16> lockOutTimerItems = { 4254, 4144, 4145, 15320 }; // Megalixir, Elixir, Hi-Elixir, Powder Boots
     auto PTarget = static_cast<CBattleEntity*>(state.GetTarget());
     auto PItem = static_cast<CItemUsable*>(state.GetItem());
 
