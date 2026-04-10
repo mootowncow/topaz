3b91b7eb2d00d47afce326a21203373679a1d494
Shiyo <strikinginsanity@hotmail.com>
2026-04-10 12:45:32 -0500

    Malediction HP / MP / TP drain

--- scripts/globals/mobskills/malediction.lua
+++ scripts/globals/mobskills/malediction.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------
 -- Malediction
--- Steals an enemy's HP. Ineffective against undead.
+-- Steals an enemy's HP, MP and TP. Ineffective against undead.
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/status")
@@ -13,6 +13,8 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = math.floor(mob:getMainLvl() * 4)
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
+    MobDrainMove(mob, target, skill, MOBDRAIN_MP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK)
+    MobDrainMove(mob, target, skill, MOBDRAIN_TP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK)
     skill:setMsg(MobDrainMove(mob, target, skill, MOBDRAIN_HP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK))
 
     return dmg
