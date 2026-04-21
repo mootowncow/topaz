66e04d8f0c00af8d73fd43ab96861e39c4847fa0
Shiyo <strikinginsanity@hotmail.com>
2026-04-21 17:01:54 -0500

    (Trust) Kupi closer standing and Shantotto nuking logic

--- scripts/globals/spells/trust/shantotto.lua
+++ scripts/globals/spells/trust/shantotto.lua
@@ -53,11 +53,12 @@ function onMobSpawn(mob)
     mob:addSimpleGambit(ai.t.CASTER, ai.c.STATUS, tpz.effect.SLEEP_II, ai.r.MA, ai.s.SPECIFIC, tpz.magic.spell.CURE)
     mob:addSimpleGambit(ai.t.CASTER, ai.c.STATUS, tpz.effect.LULLABY, ai.r.MA, ai.s.SPECIFIC, tpz.magic.spell.CURE)
 
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.CAN_DRAIN, 100, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.DRAIN)
-
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.CAN_ASPIR, 75, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.ASPIR)
-
     mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_SC_AVAILABLE, 0, ai.r.MA, ai.s.BEST_AGAINST_TARGET, tpz.magic.spellFamily.NONE)
+
+    -- Aspir / Drain
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.CAN_DRAIN, 50, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.DRAIN)
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.CAN_ASPIR, 25, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.ASPIR)
+
 
     mob:setMobMod(tpz.mobMod.TRUST_DISTANCE, 18)
 
